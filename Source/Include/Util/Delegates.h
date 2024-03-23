0a6d7505e959b06f766a7ede1c17d48e95520880
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-23 19:28:14 +0100

    Misc updates

--- Source/Include/Util/Delegates.h
+++ Source/Include/Util/Delegates.h
@@ -4,18 +4,21 @@
 #define WINCOPIES_DELEGATES_H
 
 #include "UtilBase.h"
+#include "Misc.h"
 
 namespace WinCopies
 {
 	namespace Delegates
 	{
-		TEMPLATE DLLEXPORT T Self(T value) { return value; }
-		TEMPLATE2 DLLEXPORT T2 Self(T1 value) { return value; }
+		TEMPLATE_N(2) DLLEXPORT INLINE_METHOD_RETURN(T2, Self, value, T1 value)
+		TEMPLATE using Self = Self<T, T>;
 
-		TEMPLATE DLLEXPORT inline void Increment(T& value) { ++value; }
-		TEMPLATE DLLEXPORT inline T& IncrementByRef(T& value) { return ++value; }
-		TEMPLATE DLLEXPORT inline void Decrement(T& value) { --value; }
-		TEMPLATE DLLEXPORT inline T& DecrementByRef(T& value) { return --value; }
+#define __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator) CONCATENATE(prefix, ACTION)(methodName, _operator, T& value) \
+CONCATENATE(prefix, RETURN)(T&, methodName, _operator, T& value)
+#define _WINCOPIES_DELEGATES_INC_DEC(prefix, _operator) __WINCOPIES_DELEGATES_INC_DEC(TEMPLATE DLLEXPORT inline INLINE_METHOD_, CONCATENATE(prefix, crement), SURROUND(_operator, _operator, value))
+
+			_WINCOPIES_DELEGATES_INC_DEC(In, +)
+			_WINCOPIES_DELEGATES_INC_DEC(De, -)
 	}
 }
 
