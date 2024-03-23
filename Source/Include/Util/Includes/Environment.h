ee67b425bd9888cdff1d65f6849f2890459e1722
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-23 20:05:42 +0100

    Add environment macros

--- Source/Include/Util/Includes/Environment.h
+++ Source/Include/Util/Includes/Environment.h
@@ -55,16 +55,56 @@
 
 #define ABSTRACT abstract
 
-#define HAS_CXX23 _HAS_CXX23
+#define HAS_CPP23 _HAS_CXX23
 
-#if HAS_CXX23
+#if HAS_CPP23
 #define WINDOWS_CXX23
+#define WINDOWS_CPP23
+
+#define HAS_WINDOWS_CPP23 1
+#define HAS_WINDOWS_CXX23 1
 #else
+#define WINDOWS_NOT_CPP23
 #define WINDOWS_NOT_CXX23
 #endif
 #elif HAS_GCC
 #define DLLEXPORT __attribute__((visibility("default")))
 #define DLLIMPORT
 #endif
+
+#if !HAS_WINDOWS
+#define HAS_WINDOWS_CPP23 0
+#define HAS_WINDOWS_CXX23 0
+#endif
+
+#if __cplusplus >= 201402L
+#ifndef HAS_CPP14
+#define HAS_CPP14
+#endif
+
+#ifndef HAS_CXX14
+#define HAS_CXX14
+#endif
+
+#if __cplusplus >= 201703L
+#ifndef HAS_CPP17
+#define HAS_CPP17
+#endif
+
+#ifndef HAS_CXX17
+#define HAS_CXX17
+#endif
+
+#if __cplusplus >= 202002L
+#ifndef HAS_CPP20
+#define HAS_CPP20
+#endif
+
+#ifndef HAS_CXX20
+#define HAS_CXX20
+#endif
+#endif // HAS_CPP20
+#endif // HAS_CPP17
+#endif // HAS_CPP14
 
 #endif // WINCOPIES_UTIL_ENVIRONMENT_H
