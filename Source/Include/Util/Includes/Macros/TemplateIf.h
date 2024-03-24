5878996e884cd8cc0035996b857b02f036f00399
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 00:02:50 +0100

    Update TemplateIf macros

--- Source/Include/Util/Includes/Macros/TemplateIf.h
+++ Source/Include/Util/Includes/Macros/TemplateIf.h
@@ -3,36 +3,39 @@
 #ifndef WINCOPIES_UTIL_TEMPLATE_IF_H
 #define WINCOPIES_UTIL_TEMPLATE_IF_H
 
+#include "../Typing/EnableIf.h"
+
 using namespace WinCopies;
 
-#define __TEMPLATE_IF(typeKind, ...) __TEMPLATE(typeKind, SINGLE_ARG(, __VA_ARGS__ = true))
+#define ___TEMPLATE_IF(typeKind, ...) __TEMPLATE(typeKind, SINGLE_ARG(, __VA_ARGS__ = true))
+#define __TEMPLATE_IF(typeKind, condition, ...) ___TEMPLATE_IF(typeKind, VA_APPEND(, __VA_ARGS__) ::WinCopies::Typing::EnableIf##condition<T>)
 
-#define _TEMPLATE_IF(typeKind, condition) __TEMPLATE_IF(typeKind, SINGLE_ARG(::std::enable_if_t<condition, bool>))
-#define TEMPLATE_IF(condition) _TEMPLATE_IF(class, condition)
+#define _TEMPLATE_IF(typeKind, condition, ...) __TEMPLATE_IF(typeKind, SINGLE_ARG(EnableIf<condition, bool>), __VA_ARGS__)
+#define TEMPLATE_IF(condition, ...) _TEMPLATE_IF(class, condition, __VA_ARGS__)
 
-#define __TEMPLATE_IF_T(typeKind, ...) __TEMPLATE_IF(typeKind, __VA_ARGS__)
-#define _TEMPLATE_IF_T(...) __TEMPLATE_IF_T(class, __VA_ARGS__<T>)
+#define __TEMPLATE_IF_T(typeKind, condition, ...) __TEMPLATE_IF(typeKind, condition, __VA_ARGS__)
+#define _TEMPLATE_IF_T(condition, ...) __TEMPLATE_IF_T(class, condition, __VA_ARGS__)
 
-#define TEMPLATE_IF_SIGNED _TEMPLATE_IF_T(EnableIfSigned)
-#define TEMPLATE_IF_UNSIGNED _TEMPLATE_IF_T(EnableIfUnsigned)
+#define TEMPLATE_IF_SIGNED _TEMPLATE_IF_T(Signed)
+#define TEMPLATE_IF_UNSIGNED _TEMPLATE_IF_T(Unsigned)
 
-#define TEMPLATE_IF_INTEGRAL _TEMPLATE_IF_T(EnableIfIntegral)
-#define TEMPLATE_IF_SIGNED_INTEGRAL _TEMPLATE_IF_T(EnableIfSignedIntegral)
-#define TEMPLATE_IF_UNSIGNED_INTEGRAL _TEMPLATE_IF_T(EnableIfUnsignedIntegral)
+#define TEMPLATE_IF_INTEGRAL _TEMPLATE_IF_T(Integral)
+#define TEMPLATE_IF_SIGNED_INTEGRAL _TEMPLATE_IF_T(SignedIntegral)
+#define TEMPLATE_IF_UNSIGNED_INTEGRAL _TEMPLATE_IF_T(UnsignedIntegral)
 
-#define TEMPLATE_IF_FLOAT _TEMPLATE_IF_T(EnableIfFloat)
-#define TEMPLATE_IF_ENUM _TEMPLATE_IF_T(EnableIfEnum)
+#define TEMPLATE_IF_FLOAT _TEMPLATE_IF_T(Float)
+#define TEMPLATE_IF_ENUM _TEMPLATE_IF_T(Enum)
 
 
 
-#define MULTI_TEMPLATE_IF_SIGNED(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfSigned))
-#define MULTI_TEMPLATE_IF_UNSIGNED(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfUnsigned))
+#define MULTI_TEMPLATE_IF_SIGNED(...) _TEMPLATE_IF_T(Signed, __VA_ARGS__)
+#define MULTI_TEMPLATE_IF_UNSIGNED(...) _TEMPLATE_IF_T(Unsigned, __VA_ARGS__)
 
-#define MULTI_TEMPLATE_IF_INTEGRAL(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfIntegral))
-#define MULTI_TEMPLATE_IF_SIGNED_INTEGRAL(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfSignedIntegral))
-#define MULTI_TEMPLATE_IF_UNSIGNED_INTEGRAL(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfUnsignedIntegral))
+#define MULTI_TEMPLATE_IF_INTEGRAL(...) _TEMPLATE_IF_T(Integral, __VA_ARGS__)
+#define MULTI_TEMPLATE_IF_SIGNED_INTEGRAL(...) _TEMPLATE_IF_T(SignedIntegral, __VA_ARGS__)
+#define MULTI_TEMPLATE_IF_UNSIGNED_INTEGRAL(...) _TEMPLATE_IF_T(UnsignedIntegral, __VA_ARGS__)
 
-#define MULTI_TEMPLATE_IF_FLOAT(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfFloat))
-#define MULTI_TEMPLATE_IF_ENUM(...) _TEMPLATE_IF_T(SINGLE_ARG(__VA_ARGS__, EnableIfEnum))
+#define MULTI_TEMPLATE_IF_FLOAT(...) _TEMPLATE_IF_T(Float, __VA_ARGS__)
+#define MULTI_TEMPLATE_IF_ENUM(...) _TEMPLATE_IF_T(Enum, __VA_ARGS__)
 
 #endif
