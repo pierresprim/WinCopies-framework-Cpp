9146193ca0cdf85650ba58fd69fd2dc6a4754b72
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 00:07:26 +0100

    Update OPERATOR and TEMPLATE macros

--- Source/Include/Util/Includes/Macros/Template.h
+++ Source/Include/Util/Includes/Macros/Template.h
@@ -3,23 +3,26 @@
 #ifndef WINCOPIES_UTIL_TEMPLATE_H
 #define WINCOPIES_UTIL_TEMPLATE_H
 
+#include "../../PP/Loop/For.hpp"
+#include "../Main.h"
+
 #define __TEMPLATE(typeKind, extraParameters) template<typeKind T extraParameters>
 #define _TEMPLATE(typeKind) __TEMPLATE(typeKind, )
 
 #define TEMPLATE _TEMPLATE(class)
-#define TEMPLATE2 template<class T1, class T2>
-#define TEMPLATE3 template<class T1, class T2, class T3>
-#define TEMPLATE4 template<class T1, class T2, class T3, class T4>
-#define TEMPLATE5 template<class T1, class T2, class T3, class T4, class T5>
+#define TEMPLATE_N(n, templateType) template<FOR_I(n, SURROUND, class T, )>
+#define TEMPLATE_NC(n) TEMPLATE_N(n, class)
 
 #define MULTI_TEMPLATE _TEMPLATE(class...)
 
 #define METHOD_TEMPLATE template<class F>
 
-#define OPERATOR constexpr auto operator
+#define CEAOPERATOR constexpr auto operator
 
-#define OPERATOR_TEMPLATE TEMPLATE OPERATOR
-#define OPERATOR_TEMPLATE2 TEMPLATE2 OPERATOR
+#define OPERATOR_TEMPLATE TEMPLATE CEAOPERATOR
+#define _OPERATOR_TEMPLATE_N(n, templateType, templateMacro) templateMacro(n,  templateType) CEAOPERATOR
+#define OPERATOR_TEMPLATE_N(n, templateType) TEMPLATE_N(n, templateType) CEAOPERATOR
+#define OPERATOR_TEMPLATE_NC(n) OPERATOR_TEMPLATE_N(n, class)
 
 // Named templates
 
