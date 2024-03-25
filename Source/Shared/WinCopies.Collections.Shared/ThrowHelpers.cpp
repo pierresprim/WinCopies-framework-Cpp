d368faac559103651b01a1167bcb9d3f5939471a
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 02:17:27 +0100

    Add new file

--- Source/Shared/WinCopies.Collections.Shared/ThrowHelpers.cpp
+++ Source/Shared/WinCopies.Collections.Shared/ThrowHelpers.cpp
@@ -1,12 +1,10 @@
 #include "pch.h"
 #include "../../Include/Collections/ThrowHelpers.h"
+#include "../../Include/Util/Misc.h"
 
 using namespace WinCopies::Collections;
 
-ArgumentException* ThrowHelpers::GetNotContainedLinkedListNodeException(const wchar_t* const argumentName)
-{
-	return new ArgumentException(L"The given node is not contained in the current list.", argumentName);
-}
+INLINE_OBJECT_RETURN(ArgumentException, ThrowHelpers::GetNotContainedLinkedListNodeException, const wchar_t* const argumentName, L"The given node is not contained in the current list." COMMA argumentName)
 
 ArgumentException* ThrowHelpers::GetNodesAreEqualException(const wchar_t* const argumentName)
 {
---
+++ Source/Shared/WinCopies.Util.Shared/Types/Usings.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "../../../Include/Util/Includes/Types/Usings.h"
