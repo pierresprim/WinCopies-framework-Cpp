c510e5f55beb260ca3b58bd32de2f227e5ec0352
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 01:56:46 +0100

    Update conditional macros

--- Source/Include/Util/PP/Conditional/Conditional.hpp
+++ Source/Include/Util/PP/Conditional/Conditional.hpp
@@ -5,15 +5,6 @@
 
 #include "../Math/Math.hpp"
 
-#define IF_B(value, ifTrue, ...) CONCATENATE(IF, value)(ifTrue, __VA_ARGS__)
-#define IF0(value, ...) __VA_ARGS__
-#define IF1(value, ...) value
-#define IF2(value, ...) value, __VA_ARGS__
-
-#define GET_FIRST_ARG(...) IF1(__VA_ARGS__)
-#define GET_SECOND_ARG(...) IF0(__VA_ARGS__)
-#define GET_FIRST_AND_SECOND_ARG(...) IF2(__VA_ARGS__)
-
 #define IF(value, ifTrue, ...) IF_B(BOOL(value), ifTrue, __VA_ARGS__)
 
 #define LESS(x, y) IF(x, BOOL(SUB(y, x)), BOOL(y))
--- Source/Include/Util/PP/MiscBase.hpp
+++ Source/Include/Util/PP/MiscBase.hpp
@@ -6,8 +6,28 @@
 #define CONCATENATE(value, ...) value##__VA_ARGS__
 #define SINGLE_ARG(...) __VA_ARGS__
 
-#define FIRST_ARG(first, ...) first
-#define THIRD_ARG(first, second, third, ...) third
+#define IF_B(value, ifTrue, ...) CONCATENATE(IF, value)(ifTrue, __VA_ARGS__)
+#define IF0(value, ...) __VA_ARGS__
+#define IF1(value, ...) value
+#define IF2(value, ...) value, __VA_ARGS__
+
+#define FIRST_ARG(value, ...) IF1(value, __VA_ARGS__)
+#define SECOND_ARG(x, y, ...) y
+#define THIRD_ARG(x, y, z, ...) z
+
+#define ALL_BUT_FIRST_ARG(value, ...) IF0(value, __VA_ARGS__)
+#define FIRST_TWO_ARGS(x, y, ...) x, y
+#define FIRST_THIRD_ARGS(x, y, z, ...) x, y, z
+#define ALL_ARGS(value, ...) IF2(value, __VA_ARGS__)
+
+#define GET_FIRST_ARG(...) FIRST_ARG(__VA_ARGS__)
+#define GET_SECOND_ARG(...) SECOND_ARG(__VA_ARGS__)
+#define GET_THIRD_ARG(...) THIRD_ARG(__VA_ARGS__)
+
+#define REMOVE_FIRST_ARG(...) ALL_BUT_FIRST_ARG(__VA_ARGS__)
+#define GET_FIRST_TWO_ARGS(...) FIRST_TWO_ARGS(__VA_ARGS__)
+#define GET_FIRST_THIRD_ARGS(...) FIRST_THIRD_ARGS(__VA_ARGS__)
+#define GET_ALL_ARGS(...) ALL_ARGS(__VA_ARGS__)
 
 #define _HAS_VA_OPT(...) THIRD_ARG(__VA_OPT__(,), 1, 0, )
 #define HAS_VA_OPT _HAS_VA_OPT()
