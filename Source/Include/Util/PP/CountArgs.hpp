e3ad73340afc2c408d897552005625ab8028580a
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 01:52:58 +0100

    Add new macros for variadic arguments

--- Source/Include/Util/PP/CountArgs.hpp
+++ Source/Include/Util/PP/CountArgs.hpp
@@ -19,8 +19,16 @@
 #define VA_ARGS_EMPTY(...) NOT(VA_ARGS_FILLED(__VA_ARGS__))
 
 #define VA_OPT(value, ...) IF(VA_ARGS_FILLED(__VA_ARGS__), value)
+#define VA_ADD(value, macro, ...) IF(VA_ARGS_FILLED(__VA_ARGS__), macro(value, __VA_ARGS__))
+#define VA_PREPEND(value, ...) IF(VA_ARGS_EMPTY(__VA_ARGS__),, value, __VA_ARGS__)
+#define VA_APPEND(value, ...) IF(VA_ARGS_EMPTY(__VA_ARGS__),, __VA_ARGS__, value)
+#define VA_SURROUND(prefix, suffix, macro, ...) IF(VA_ARGS_FILLED(__VA_ARGS__), macro(prefix, suffix, __VA_ARGS__))
 #else
 #define VA_OPT(value, ...) __VA_OPT__(value)
+#define VA_ADD(value, macro, ...) __VA_OPT__(macro(value, __VA_ARGS__))
+#define VA_PREPEND(value, ...) __VA_OPT__(value, __VA_ARGS__)
+#define VA_APPEND(value, ...) __VA_OPT__(__VA_ARGS__, value)
+#define VA_SURROUND(prefix, suffix, macro, ...) __VA_OPT__(macro(prefix, suffix, __VA_ARGS__))
 
 #define COUNT_ARGS(...) _COUNT_ARGS(__VA_ARGS__)
 
