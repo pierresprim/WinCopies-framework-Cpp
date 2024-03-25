529145e7c2b212266cfcf226a366eebf18c7c6ed
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-03-25 01:10:06 +0100

    Misc updates

--- Source/Include/Util/Includes/Macros/Type/Type.h
+++ Source/Include/Util/Includes/Macros/Type/Type.h
@@ -9,10 +9,11 @@
 
 #define BASE_INTERFACE public virtual
 
-#define INTERFACE_CLASS(interfaceName) CLASS interfaceName ABSTRACT
 #define CLASS class DLLEXPORT
+#define INTERFACE_CLASS(interfaceName) CLASS interfaceName ABSTRACT
 #define STRUCT struct DLLEXPORT
 #define ENUM enum CLASS
+#define CONST_EXPR DLLEXPORT constexpr
 
 #define SHARED_TYPE(typeKind, typeName, type) typeKind typeName : public ::std::enable_shared_from_this<typeName type>
 #define _SHARED_CLASS(className, type) SHARED_TYPE(CLASS, className, type)
@@ -25,26 +26,5 @@
 #include "Templates/Struct.h"
 #include "Templates/Interface.h"
 #include "Templates/Class.h"
-
-#define _TO_UNDERLYING(value, methodNamespace) methodNamespace to_underlying(value)
-
-#ifdef WINDOWS
-#if !_HAS_CXX23
-template <class enum_t>
-constexpr ::std::underlying_type_t<enum_t> to_underlying(enum_t value) noexcept {
-	return static_cast<::std::underlying_type_t<enum_t>>(value);
-}
-
-#define TO_UNDERLYING(value) _TO_UNDERLYING(value, )
-#endif
-#endif
-
-#ifndef TO_UNDERLYING
-#define TO_UNDERLYING(value) _TO_UNDERLYING(value, ::std::)
-#endif
-
-#define ENUM_CAST(enumType, enumValue) static_cast<std::underlying_type_t<enumType>>(enumValue)
-
-#define IS_ENUM(type) ::std::is_enum_v<type>
 
 #endif
