b22e8f7f58dd88e1aaa77751129c373bb9288477
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-04-01 04:52:36 +0200

    Update variadic processing macros

--- Source/Include/Util/PP/Util.hpp
+++ Source/Include/Util/PP/Util.hpp
@@ -13,12 +13,13 @@
 #define GET_ARGS(start, length, ...) SELECT_ARGS(start, length, __VA_ARGS__)
 #define GET_ARG(i, ...) GET_ARGS(i, 1, __VA_ARGS__)
 
-#define CONCATENATE_ARGS(...) 
+//#define CONCATENATE_ARGS(...) 
 #define JOIN_ARGS(separator, ...) FOR_EACH(TRANSCRIBE_ARGS, separator, SINGLE_ARG, , __VA_ARGS__)
 #define CONCATENATE_WITH(concatenator, prefix, suffix, ...) FOR_EACH(concatenator, prefix, SINGLE_ARG, suffix, __VA_ARGS__)
-#define SURROUND_ARGS(concatenator, prefix, suffix, ...) CONCATENATE_WITH(concatenator, prefix, suffix, __VA_ARGS__)
+#define SURROUND_ARGS(prefix, suffix, ...) CONCATENATE_WITH(SURROUND, prefix, suffix, __VA_ARGS__)
+#define TRANSCRIBE_ARGS_WITH(prefix, suffix, ...) CONCATENATE_WITH(TRANSCRIBE_ARGS, prefix, suffix, __VA_ARGS__)
 
-#define TYPE_TEMPLATE_N(n, kind, ...) template<FFOR(n, SURROUND, kind T, SINGLE_ARG) __VA_OPT__(,) SURROUND_ARGS(TRANSCRIBE_ARGS, kind, , __VA_ARGS__)>
+#define TYPE_TEMPLATE_N(n, kind, ...) template<FFOR(n, SURROUND, kind T, SINGLE_ARG) __VA_OPT__(,)  TRANSCRIBE_ARGS_WITH(kind, , __VA_ARGS__)>
 #define TEMPLATE_NE(n, ...) TYPE_TEMPLATE_N(n, class, __VA_ARGS__)
 #define TEMPLATE_TYPES(n) FFOR(n, SURROUND, T, SINGLE_ARG)
 
