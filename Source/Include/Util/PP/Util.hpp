699f1eece60b6ddb3df565b27a7e4ff12c29c0b4
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-05-19 16:00:15 +0200

    Add macros and bug fixes

--- Source/Include/Util/PP/Util.hpp
+++ Source/Include/Util/PP/Util.hpp
@@ -9,9 +9,15 @@
 #define SELECT_ARGS_FROM(i, ...) SELECT(i, 1, SURROUND, , DISCARD, , __VA_ARGS__)
 #define GET_ARGS_FROM(i, ...) SELECT_ARGS_FROM(i, __VA_ARGS__)
 
-#define SELECT_ARGS(start, length, ...) SELECT(length, 0, SURROUND, , SINGLE_ARG, , GET_ARGS_FROM(start, __VA_ARGS__))
+#define _SELECT_ARGS(length, ...) FIRST_ARG(__VA_ARGS__) SELECT(DECREMENT(length), 0, TRANSCRIBE_ARGS_CS, , SINGLE_ARG, , ALL_BUT_FIRST_ARG(__VA_ARGS__))
+#define SELECT_ARGS(start, length, ...) _SELECT_ARGS(length, GET_ARGS_FROM(start, __VA_ARGS__))
 #define GET_ARGS(start, length, ...) SELECT_ARGS(start, length, __VA_ARGS__)
+
+#define SELECT_ARG(i, ...) SELECT_ARGS(i, 1, __VA_ARGS__)
 #define GET_ARG(i, ...) GET_ARGS(i, 1, __VA_ARGS__)
+
+#define SELECT_N_ARGS(length, ...) SELECT_ARGS(0, length, __VA_ARGS__)
+#define GET_N_ARGS(length, ...) SELECT_N_ARGS(length, __VA_ARGS__)
 
 //#define CONCATENATE_ARGS(...) 
 #define _JOIN_ARGS(separator, first, ...) first FOR_EACH_C(TRANSCRIBE_ARGS, separator, , __VA_ARGS__)
