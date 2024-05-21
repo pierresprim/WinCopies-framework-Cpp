4edd313b37b633759453fc8175223e1e5ffc22cd
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-05-22 01:52:03 +0200

    New macros

--- Source/Include/Util/PP/Conditional/Conditional.hpp
+++ Source/Include/Util/PP/Conditional/Conditional.hpp
@@ -3,7 +3,7 @@
 #ifndef WINCOPIES_CONDITIONAL_HPP
 #define WINCOPIES_CONDITIONAL_HPP
 
-#include "../Math/MathBase.hpp"
+#include "../Math/Base.hpp"
 
 #define LESS(x, y) IF(x, BOOL(SUB(y, x)), BOOL(y))
 
@@ -11,6 +11,10 @@
 
 #define EQUAL(x, y) _EQUAL_OR_GREATER(x, y, NOT)
 #define GREATER(x, y) _EQUAL_OR_GREATER(x, y, SINGLE_ARG)
+
+#define IF_LESS(x, y, ifTrue, ...) IF_B(LESS(x, y), ifTrue, __VA_ARGS__)
+#define EQUALS(x, y, ifTrue, ...) IF_B(EQUAL(x, y), ifTrue, __VA_ARGS__)
+#define IF_GREATER(x, y, ifTrue, ...) IF_B(GREATER(x, y), ifTrue, __VA_ARGS__)
 
 #define SURROUND_IF_ELSE(condition, prefix, ifTrue, suffix, ...) IF(condition, SURROUND(prefix, ifTrue, suffix), __VA_ARGS__)
 #define SURROUND_IF(condition, prefix, value, ...) SURROUND_IF_ELSE(condition, prefix, value, __VA_ARGS__, value)
