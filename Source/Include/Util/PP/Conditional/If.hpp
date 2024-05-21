a5e1dd13771450d153e74471653882f7a95f7e70
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-05-22 01:53:13 +0200

    New macros

--- Source/Include/Util/PP/Conditional/If.hpp
+++ Source/Include/Util/PP/Conditional/If.hpp
@@ -3,8 +3,17 @@
 #ifndef WINCOPIES_IF_HPP
 #define WINCOPIES_IF_HPP
 
-#include "Bool.hpp"
+#include "../MISC.hpp"
+#include "Tables.h"
 
 #define IF(value, ifTrue, ...) IF_B(BOOL(value), ifTrue, __VA_ARGS__)
+
+#define IF_VALUES(_operator, x, y) SURROUND(_operator, x, y)
+
+#define AND_B(x, y) IF_VALUES(AND, x, y)
+#define OR_B(x, y) IF_VALUES(OR, x, y)
+
+#define AND(x, y) AND_B(BOOL(x), BOOL(y))
+#define OR(x, y) OR_B(BOOL(x), BOOL(y))
 
 #endif // WINCOPIES_IF_HPP
---
+++ Source/Include/Util/PP/Conditional/Tables.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef WINCOPIES_TRUTH_TABLES_HPP
+#define WINCOPIES_TRUTH_TABLES_HPP
+
+#define AND00 0
+#define AND01 0
+#define AND10 0
+#define AND11 1
+
+#define OR00 0
+#define OR01 1
+#define OR10 1
+#define OR11 1
+
+#endif // WINCOPIES_TRUTH_TABLES_HPP
