fb1a32b0effede5e78cb13dadd03ac2daf862ca3
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-05-23 01:44:44 +0200

    Update typedefs

--- Source/Include/Util/Includes/Types/Defs.h
+++ Source/Include/Util/Includes/Types/Defs.h
@@ -3,26 +3,11 @@
 #ifndef WINCOPIES_UTIL_TYPE_DEFS_H
 #define WINCOPIES_UTIL_TYPE_DEFS_H
 
-#define _TYPEDEF(value) typedef value
-#define TYPEDEF_U _TYPEDEF(unsigned)
-#define _TYPEDEF_L(macro) macro long
-#define TYPEDEF_UL _TYPEDEF_L(TYPEDEF_U)
-#define TYPEDEF_L _TYPEDEF_L(typedef)
-
-typedef char SBYTE;
-TYPEDEF_U short USHORT;
-TYPEDEF_U int UINT;
-TYPEDEF_UL ULONG;
-TYPEDEF_UL long ULONGLONG;
+#include "DefsBase.h"
 
 typedef char* STRING;
 typedef wchar_t* WSTRING;
 typedef std::wstring STDSTRING;
 typedef std::string STDSTRINGA;
-
-#ifndef WINDOWS
-TYPEDEF_L LONG;
-TYPEDEF_U char BYTE;
-#endif
 
 #endif // WINCOPIES_UTIL_TYPE_DEFS_H
---
+++ Source/Include/Util/Includes/Types/DefsBase.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef WINCOPIES_UTIL_TYPE_DEFS_BASE_H
+#define WINCOPIES_UTIL_TYPE_DEFS_BASE_H
+
+#define _TYPEDEF(value) typedef value
+#define TYPEDEF_U _TYPEDEF(unsigned)
+#define _TYPEDEF_L(macro) macro long
+#define TYPEDEF_UL _TYPEDEF_L(TYPEDEF_U)
+#define TYPEDEF_L _TYPEDEF_L(typedef)
+
+typedef char SBYTE;
+TYPEDEF_U short USHORT;
+TYPEDEF_U int UINT;
+TYPEDEF_UL ULONG;
+TYPEDEF_UL long ULONGLONG;
+
+#ifdef WINDOWS
+#include <Windows.h>
+#else
+TYPEDEF_L LONG;
+TYPEDEF_U char BYTE;
+#endif
+
+#endif // WINCOPIES_UTIL_TYPE_DEFS_BASE_H
