3f0fffc42fbf4346e41f40bfce3114c7e4a2fb49
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-05-27 17:23:01 +0200

    Update TEMPLATE_IF macros

--- Source/Include/Util/Includes/Macros/TemplateIf.h
+++ Source/Include/Util/Includes/Macros/TemplateIf.h
@@ -5,8 +5,6 @@
 
 #include "../Typing/EnableIf.h"
 
-using namespace WinCopies;
-
 #define ___TEMPLATE_IF(typeKind, ...) __TEMPLATE(typeKind, SINGLE_ARG(, __VA_ARGS__ = true))
 #define __TEMPLATE_IF(typeKind, condition, ...) ___TEMPLATE_IF(typeKind, VA_APPEND(, __VA_ARGS__) ::WinCopies::Typing::EnableIf##condition<T>)
 
@@ -16,26 +14,16 @@ using namespace WinCopies;
 #define __TEMPLATE_IF_T(typeKind, condition, ...) __TEMPLATE_IF(typeKind, condition, __VA_ARGS__)
 #define _TEMPLATE_IF_T(condition, ...) __TEMPLATE_IF_T(class, condition, __VA_ARGS__)
 
-#define TEMPLATE_IF_SIGNED _TEMPLATE_IF_T(Signed)
-#define TEMPLATE_IF_UNSIGNED _TEMPLATE_IF_T(Unsigned)
-
-#define TEMPLATE_IF_INTEGRAL _TEMPLATE_IF_T(Integral)
-#define TEMPLATE_IF_SIGNED_INTEGRAL _TEMPLATE_IF_T(SignedIntegral)
-#define TEMPLATE_IF_UNSIGNED_INTEGRAL _TEMPLATE_IF_T(UnsignedIntegral)
-
-#define TEMPLATE_IF_FLOAT _TEMPLATE_IF_T(Float)
-#define TEMPLATE_IF_ENUM _TEMPLATE_IF_T(Enum)
 
 
+#define TEMPLATE_IF_SIGNED(...) _TEMPLATE_IF_T(Signed, __VA_ARGS__)
+#define TEMPLATE_IF_UNSIGNED(...) _TEMPLATE_IF_T(Unsigned, __VA_ARGS__)
 
-#define MULTI_TEMPLATE_IF_SIGNED(...) _TEMPLATE_IF_T(Signed, __VA_ARGS__)
-#define MULTI_TEMPLATE_IF_UNSIGNED(...) _TEMPLATE_IF_T(Unsigned, __VA_ARGS__)
+#define TEMPLATE_IF_INTEGRAL(...) _TEMPLATE_IF_T(Integral, __VA_ARGS__)
+#define TEMPLATE_IF_SIGNED_INTEGRAL(...) _TEMPLATE_IF_T(SignedIntegral, __VA_ARGS__)
+#define TEMPLATE_IF_UNSIGNED_INTEGRAL(...) _TEMPLATE_IF_T(UnsignedIntegral, __VA_ARGS__)
 
-#define MULTI_TEMPLATE_IF_INTEGRAL(...) _TEMPLATE_IF_T(Integral, __VA_ARGS__)
-#define MULTI_TEMPLATE_IF_SIGNED_INTEGRAL(...) _TEMPLATE_IF_T(SignedIntegral, __VA_ARGS__)
-#define MULTI_TEMPLATE_IF_UNSIGNED_INTEGRAL(...) _TEMPLATE_IF_T(UnsignedIntegral, __VA_ARGS__)
-
-#define MULTI_TEMPLATE_IF_FLOAT(...) _TEMPLATE_IF_T(Float, __VA_ARGS__)
-#define MULTI_TEMPLATE_IF_ENUM(...) _TEMPLATE_IF_T(Enum, __VA_ARGS__)
+#define TEMPLATE_IF_FLOAT(...) _TEMPLATE_IF_T(Float, __VA_ARGS__)
+#define TEMPLATE_IF_ENUM(...) _TEMPLATE_IF_T(Enum, __VA_ARGS__)
 
 #endif
