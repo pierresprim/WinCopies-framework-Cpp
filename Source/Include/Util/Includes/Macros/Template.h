a5d9f2cb8ad34bc75cca7960b81b8f27325454ac
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-06-04 15:04:14 +0200

    Update some TEMPLATE macros

--- Source/Include/Util/Includes/Macros/Template.h
+++ Source/Include/Util/Includes/Macros/Template.h
@@ -6,14 +6,16 @@
 #include "../../PP/Loop/For.hpp"
 #include "../../PP/Loop/ForEach.hpp"
 #include "../../PP/Util.hpp"
+#include "../../PP/Variadic/Variadic.hpp"
 #include "../Main.h"
+#include "../Types/DefsBase.h"
 
 #define __TEMPLATE(typeKind, extraParameters) template<typeKind T extraParameters>
 #define _TEMPLATE(typeKind) __TEMPLATE(typeKind, )
 
 #define TEMPLATE _TEMPLATE(class)
-#define TEMPLATE_N(n, templateType) template<FOR_I(n, SURROUND, templateType T, )>
-#define TEMPLATE_NC(n) TEMPLATE_N(n, class)
+#define TEMPLATE_N(n, templateType, ...) template<FOR_I(n, SURROUND, templateType T, ) VA_OPT(COMMA SINGLE_ARG(__VA_ARGS__), __VA_ARGS__)>
+#define TEMPLATE_NC(n, ...) TEMPLATE_N(n, class, __VA_ARGS__)
 
 #define MULTI_TEMPLATE _TEMPLATE(class...)
 
@@ -32,5 +34,9 @@
 #define NAMED_TEMPLATE(typeName) template<class typeName>
 #define NAMED_TEMPLATE_N(templateType, ...) template<TRANSCRIBE_ARGS_WITH(templateType, , __VA_ARGS__)>
 #define NAMED_TEMPLATE_NC(...) NAMED_TEMPLATE_N(class, __VA_ARGS__)
+
+#define TEMPLATE_SPECIALIZATION(type, prefix, name) TEMPLATE using SURROUND(I, prefix, name) = I##name<T, type>
+#define COLLECTION_TEMPLATE_SPECIALIZATION(name) TEMPLATE_SPECIALIZATION(UINT, UInt, name); TEMPLATE_SPECIALIZATION(ULONGLONG, ULong, name);
+#define LIST_TEMPLATE_SPECIALIZATION(name) COLLECTION_TEMPLATE_SPECIALIZATION(name##R) COLLECTION_TEMPLATE_SPECIALIZATION(name##W) COLLECTION_TEMPLATE_SPECIALIZATION(name)
 
 #endif
