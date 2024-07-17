c7930beda234ebac005d5478a527ee1ff4a142ee
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-17 19:56:38 +0200

    New macros and improvements

--- Source/Include/Util/Includes/Macros/Template.h
+++ Source/Include/Util/Includes/Macros/Template.h
@@ -13,15 +13,22 @@
 #define __TEMPLATE(typeKind, extraParameters) template<typeKind T extraParameters>
 #define _TEMPLATE(typeKind) __TEMPLATE(typeKind, )
 
+#define MAKE_TEMPLATE_PARAMS(count, prefix, suffix) FOR_I(count, SURROUND, prefix T##suffix, )
+
 #define TEMPLATE _TEMPLATE(class)
-#define TEMPLATE_N(n, templateType, ...) template<FOR_I(n, SURROUND, templateType T, ) VA_OPT(COMMA SINGLE_ARG(__VA_ARGS__), __VA_ARGS__)>
+
+#define TEMPLATE_N(n, kind, ...) template<MAKE_TEMPLATE_PARAMS(n, kind, ) VA_OPT(IF_NOT(n, COMMA) SINGLE_ARG(__VA_ARGS__), __VA_ARGS__)>
 #define TEMPLATE_NC(n, ...) TEMPLATE_N(n, class, __VA_ARGS__)
+
+#define TEMPLATE_E(kind, ...) TEMPLATE_N(0, , TRANSCRIBE_SURROUNDED(kind, , __VA_ARGS__))
+#define TEMPLATE_EC(...) TEMPLATE_E(class, __VA_ARGS__)
+
+#define TEMPLATE_NE(n, kind, ...) TEMPLATE_N(n, kind, TRANSCRIBE_SURROUNDED(kind, , __VA_ARGS__))
+#define TEMPLATE_NEC(n, ...) TEMPLATE_NE(n, class, __VA_ARGS__)
 
 #define MULTI_TEMPLATE _TEMPLATE(class...)
 
 #define METHOD_TEMPLATE template<class F>
-
-#define MAKE_TEMPLATE_PARAMS(count, prefix, suffix) FOR_I(count, SURROUND, prefix T##suffix, )
 
 #define CEA_OPERATOR constexpr auto operator
 
@@ -35,8 +42,8 @@
 #define NAMED_TEMPLATE_N(templateType, ...) template<TRANSCRIBE_ARGS_WITH(templateType, , __VA_ARGS__)>
 #define NAMED_TEMPLATE_NC(...) NAMED_TEMPLATE_N(class, __VA_ARGS__)
 
-#define TEMPLATE_SPECIALIZATION(type, prefix, name) TEMPLATE using SURROUND(I, prefix, name) = I##name<T, type>
-#define COLLECTION_TEMPLATE_SPECIALIZATION(name) TEMPLATE_SPECIALIZATION(UINT, UInt, name); TEMPLATE_SPECIALIZATION(ULONGLONG, ULong, name);
-#define LIST_TEMPLATE_SPECIALIZATION(name) COLLECTION_TEMPLATE_SPECIALIZATION(name##R) COLLECTION_TEMPLATE_SPECIALIZATION(name##W) COLLECTION_TEMPLATE_SPECIALIZATION(name)
+#define TEMPLATE_SPECIALIZATION(isTemplate, type, prefix, name) IF(isTemplate, TEMPLATE) using SURROUND(I, prefix, name) = I##name<IF_NOT(isTemplate, T,) type>
+#define COLLECTION_TEMPLATE_SPECIALIZATION(isTemplate, name) TEMPLATE_SPECIALIZATION(isTemplate, UINT, UInt, name); TEMPLATE_SPECIALIZATION(isTemplate, ULONGLONG, ULong, name);
+#define LIST_TEMPLATE_SPECIALIZATION(name) COLLECTION_TEMPLATE_SPECIALIZATION(1, name##R) COLLECTION_TEMPLATE_SPECIALIZATION(1, name##W) COLLECTION_TEMPLATE_SPECIALIZATION(1, name)
 
 #endif
