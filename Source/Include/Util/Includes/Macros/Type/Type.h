40da028176d6015a3d2b47c1fdf25a75427395ac
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-17 20:11:34 +0200

    New macros and improvements

--- Source/Include/Util/Includes/Macros/Type/Type.h
+++ Source/Include/Util/Includes/Macros/Type/Type.h
@@ -9,23 +9,33 @@
 #include "../TemplateIf.h"
 #include "TypeBase.h"
 
-// Type kinds
-
 #define BASE_INTERFACE public virtual
 
-#define _BASE_INTERFACES(suffix, ...) BASE_INTERFACE FIRST_ARG(__VA_ARGS__)suffix FOR_EACH_C(TRANSCRIBE_ARGS_CS, BASE_INTERFACE, suffix, ALL_BUT_FIRST_ARG(__VA_ARGS__))
-#define BASE_INTERFACES(...) _BASE_INTERFACES(, __VA_ARGS__)
-#define BASE_TEMPLATES(...) _BASE_INTERFACES(<T>, __VA_ARGS__)
+#define __BASE_INTERFACES(suffix, macro, ...) SURROUND(TRANSCRIBE, macro, _SURROUNDED)(BASE_INTERFACE, suffix, __VA_ARGS__)
+#define _BASE_INTERFACES(areArgsTokens, suffix, ...) __BASE_INTERFACES(suffix, IF(areArgsTokens, _TOKENS), __VA_ARGS__)
+
+#define BASE_INTERFACES(...) _BASE_INTERFACES(0, , __VA_ARGS__)
+#define BASE_TEMPLATES(...) _BASE_INTERFACES(0, <T>, __VA_ARGS__)
+
+#define BASE_INTERFACE_TEMPLATES(...) _BASE_INTERFACES(1, , __VA_ARGS__)
 
 #define _BASE_TEMPLATE(name, ...) BASE_INTERFACE name<__VA_ARGS__>
 
 #define BASE_TEMPLATE(name) _BASE_TEMPLATE(name, T)
-#define BASE_TEMPLATE_N(name, count) _BASE_TEMPLATE(name, MAKE_TEMPLATE_PARAMS(count, , ))
+#define BASE_TEMPLATE_N(count, name) _BASE_TEMPLATE(name, TEMPLATE_TYPES(count))
 
-#define CLASS class DLLEXPORT
-#define INTERFACE_CLASS(interfaceName) CLASS interfaceName ABSTRACT
+#define STATIC_CLASS(name) INTERFACE_TYPE(name) final { STATIC_CLASS_C_D_TOR(name)
+
+#define _INTERFACE_CLASS(addCtorAndDtor, name, ...) INTERFACE_TYPE(name) : BASE_INTERFACE ::WinCopies::Interface VA_OPT(COMMA, __VA_ARGS__) __VA_ARGS__ IF(addCtorAndDtor, { INTERFACE_CONSTRUCTOR(name))
+#define INTERFACE_CLASS(name, ...) _INTERFACE_CLASS(1, name, __VA_ARGS__)
 #define STRUCT struct DLLEXPORT
 #define ENUM enum CLASS
+
+#define TEMPLATE_INTERFACE(name, macro, ...) macro() INTERFACE_CLASS(name, __VA_ARGS__)
+#define TEMPLATE_INTERFACE_NC(name, n, ...) TEMPLATE_NC(n) INTERFACE_CLASS(name, __VA_ARGS__)
+#define TEMPLATE_INTERFACE_C(name, ...) TEMPLATE INTERFACE_CLASS(name, __VA_ARGS__)
+
+#define CONSTRAINED_INTERFACE_NC(name, n, ...) TEMPLATE_NC(n, __VA_ARGS__) INTERFACE_CLASS(name)
 
 #define SHARED_TYPE(typeKind, typeName, type) typeKind typeName : public ::std::enable_shared_from_this<typeName type>
 #define _SHARED_CLASS(className, type) SHARED_TYPE(CLASS, className, type)
--- Source/Include/Util/Includes/Macros/Type/TypeBase.h
+++ Source/Include/Util/Includes/Macros/Type/TypeBase.h
@@ -7,4 +7,26 @@
 
 #define CONST_EXPR DLLEXPORT constexpr
 
+#define DEFAULT_C_D_TOR(name, modifier, implementation, ...) modifier name() __VA_ARGS__ = implementation;
+
+#define DEFAULT_CONSTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(name, , implementation, __VA_ARGS__)
+#define DEFAULT_DESTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(~name, virtual, implementation, __VA_ARGS__)
+
+#define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR(name, default)
+#define INTERFACE_DESTRUCTOR(name, ...) public: DEFAULT_DESTRUCTOR(name, 0, __VA_ARGS__)
+
+#define INTERFACE_C_D_TOR(name, ...) INTERFACE_CONSTRUCTOR(name) INTERFACE_DESTRUCTOR(name, __VA_ARGS__)
+
+#define SCOPED_INTERFACE_C_D_TOR(name, ...) { INTERFACE_C_D_TOR(name, __VA_ARGS__) };
+
+#define _STATIC_CLASS_C_D_TOR(name, prefix, ...) SURROUND(DEFAULT_, prefix, STRUCTOR)(name, delete, __VA_ARGS__)
+
+#define STATIC_CLASS_CONSTRUCTOR(name) _STATIC_CLASS_C_D_TOR(name, CON)
+#define STATIC_CLASS_DESTRUCTOR(name) _STATIC_CLASS_C_D_TOR(name, DE, final)
+
+#define STATIC_CLASS_C_D_TOR(name) private: STATIC_CLASS_CONSTRUCTOR(name) STATIC_CLASS_DESTRUCTOR(name)
+
+#define CLASS class DLLEXPORT
+#define INTERFACE_TYPE(name) CLASS name ABSTRACT
+
 #endif // WINCOPIES_UTIL_MACROS_TYPE_BASE_H
