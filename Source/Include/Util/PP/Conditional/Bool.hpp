8cfae903d1b92b5ab964cd5e9c922f401d52db71
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-17 19:44:47 +0200

    Misc updates

--- Source/Include/Util/PP/Conditional/Bool.hpp
+++ Source/Include/Util/PP/Conditional/Bool.hpp
@@ -5,26 +5,27 @@
 
 #include "../MiscBase.hpp"
 
-#define _CHECK(value, n, ...) n
-#define CHECK(n, ...) SINGLE_ARG(_CHECK(__VA_ARGS__, n,))
+#define __CHECK(value, n, ...) n
+#define _CHECK(n, ...) SINGLE_ARG(__CHECK(__VA_ARGS__, n,))
 
-#define CHECK_N(macro, value) CHECK(0, CONCATENATE(macro, value))
+#define CHECK(macro, value) _CHECK(0, CONCATENATE(macro, value))
 
-#define NOT(value) CHECK_N(NOT, value)
+#define NOT(value) CHECK(NOT, value)
 #define NOT0 , 1
 
 #define COMPL(b) CONCATENATE(COMPL, b)
 #define COMPL0 1
 #define COMPL1 0
 
-#define _BOOL(value) CHECK(1, CONCATENATE(_BOOL, value))
+#define CHECK_BOOL(value) _CHECK(1, CONCATENATE(_BOOL, value))
 #define _BOOL0 , 0
+#define _BOOL _BOOL0
 
-#define _IS_ONE1 NOT0
+#define IS_ONE1 NOT0
 
 #define IS_ZERO(value) NOT(value)
-#define IS_ONE(value) CHECK_N(_IS_ONE, value)
+#define IS_ONE(value) CHECK(IS_ONE, value)
 
-#define BOOL(value) _BOOL(value)
+#define BOOL(value) CHECK_BOOL(value)
 
 #endif // WINCOPIES_BOOL_HPP
