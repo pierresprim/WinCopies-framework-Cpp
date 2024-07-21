3edf349b53d4d7d220acb5e95fde64b30a3186ee
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-21 23:22:33 +0200

    Add macro

--- Source/Include/Util/PP/Math/Base.hpp
+++ Source/Include/Util/PP/Math/Base.hpp
@@ -8,4 +8,6 @@
 #define ADD(x, y) REPEAT(y, INCREMENT, x)
 #define SUB(x, y) REPEAT(y, DECREMENT, x)
 
+#define SUB_OR_DISCARD_IF_ZERO(n) IF(n, DECREMENT, DISCARD)(n)
+
 #endif // WINCOPIES_MATH_BASE_HPP
