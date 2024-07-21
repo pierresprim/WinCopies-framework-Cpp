dba681fb37d0c50a5b6482e4becdda318f7527ce
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-22 00:59:13 +0200

    Update Consts.h

--- Source/Include/Util/Includes/Typing/Consts.h
+++ Source/Include/Util/Includes/Typing/Consts.h
@@ -7,24 +7,27 @@
 #include "../Macros/Template.h"
 #include "../Macros/Type/TypeBase.h"
 #include "Defines/Consts.h"
-#include "EnableIf.h"
 #include "Enum.h"
 
 using namespace std;
+
+#define __ENABLE_IF_BOOL_CONDITION inline CONST_EXPR bool
+#define _ENABLE_IF_BOOL_CONDITION(...) TEMPLATE_EC(__VA_ARGS__) __ENABLE_IF_BOOL_CONDITION
+#define ENABLE_IF_BOOL_CONDITION TEMPLATE __ENABLE_IF_BOOL_CONDITION
 
 namespace WinCopies
 {
 	namespace Typing
 	{
-		TEMPLATE inline CONST_EXPR bool IsSigned = is_signed_v<T>;
-		TEMPLATE inline CONST_EXPR bool IsUnsigned = is_unsigned_v<T>;
+		ENABLE_IF_BOOL_CONDITION IsSigned = is_signed_v<T>;
+		ENABLE_IF_BOOL_CONDITION IsUnsigned = is_unsigned_v<T>;
 
-		TEMPLATE inline CONST_EXPR bool IsIntegral = is_integral_v<T>;
-		TEMPLATE inline CONST_EXPR bool IsSignedIntegral = IsIntegral<T> && IsSigned<T>;
-		TEMPLATE inline CONST_EXPR bool IsUnsignedIntegral = IsIntegral<T> && IsUnsigned<T>;
+		ENABLE_IF_BOOL_CONDITION IsIntegral = is_integral_v<T>;
+		ENABLE_IF_BOOL_CONDITION IsSignedIntegral = IsIntegral<T> && IsSigned<T>;
+		ENABLE_IF_BOOL_CONDITION IsUnsignedIntegral = IsIntegral<T> && IsUnsigned<T>;
 
-		TEMPLATE inline CONST_EXPR bool IsFloat = is_floating_point_v<T>;
-		TEMPLATE inline CONST_EXPR bool IsEnum = is_enum_v<T>;
+		ENABLE_IF_BOOL_CONDITION IsFloat = is_floating_point_v<T>;
+		ENABLE_IF_BOOL_CONDITION IsEnum = is_enum_v<T>;
 
 		TEMPLATE struct IsBitwiseEnumType;
 
@@ -34,8 +37,8 @@ namespace WinCopies
 		 *
 		 * @tparam T The enum type on which the type trait is being queried.
 		 */
-		TEMPLATE inline CONST_EXPR bool IsBitwiseEnum = IsBitwiseEnumType<T>::value;
-		TEMPLATE inline CONST_EXPR bool IsUnsignedEnum = IsEnum<T> && IsUnsigned<UNDERLYING_TYPE(T)>;
+		ENABLE_IF_BOOL_CONDITION IsBitwiseEnum = IsBitwiseEnumType<T>::value;
+		ENABLE_IF_BOOL_CONDITION IsUnsignedEnum = IsEnum<T> && IsUnsigned<UNDERLYING_TYPE(T)>;
 	}
 }
 
