fc79b6c6ea9c857ad7ccde9e6ec8f712079c37df
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-25 20:06:27 +0200

    Bug fix and new macro

--- Source/Include/Util/PP/Variadic/Util.hpp
+++ Source/Include/Util/PP/Variadic/Util.hpp
@@ -8,9 +8,7 @@
 
 #define GET_LAST_ARG_INDEX(...) SUB_OR_DISCARD_IF_ZERO(COUNT_ARGS(__VA_ARGS__))
 
-#define __GET_LAST_ARG(...) GET_ARG(GET_LAST_ARG_INDEX(__VA_ARGS__), __VA_ARGS__)
-#define _GET_LAST_ARG(n, ...) 
-#define GET_LAST_ARG(...) _GET_LAST_ARG(COUNT_ARGS(__VA_ARGS__), __VA_ARGS__)
+#define GET_LAST_ARG(...) GET_ARG(GET_LAST_ARG_INDEX(__VA_ARGS__), __VA_ARGS__)
 
 #define ____GET_ARG_INDEX_FROM_LAST(i, n) IF_LESS(i, n, 0)
 #define ___GET_ARG_INDEX_FROM_LAST(i, j, k) IF(j, j, ____GET_ARG_INDEX_FROM_LAST(i, k))
@@ -21,5 +19,7 @@
 #define GET_ARG_FROM_LAST(i, ...) GET_ARG(GET_ARG_INDEX_FROM_LAST(i, __VA_ARGS__), __VA_ARGS__)
 
 #define GET_ARGS_FROM_LAST(start, length, ...) GET_ARGS(GET_ARG_INDEX_FROM_LAST(start, __VA_ARGS__), length, __VA_ARGS__)
+
+#define GET_ARGS_RTL(start, length, ...) GET_ARGS(SUB(GET_ARG_INDEX_FROM_LAST(start, __VA_ARGS__), DECREMENT(length)), length, __VA_ARGS__)
 
 #endif WINCOPIES_VA_UTIL_HPP
