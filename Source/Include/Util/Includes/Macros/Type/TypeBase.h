615c8243036b10ec308cc59a7ca6ff777a95b9f5
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-26 15:33:27 +0200

    Update some C/DTOR macros

--- Source/Include/Util/Includes/Macros/Type/TypeBase.h
+++ Source/Include/Util/Includes/Macros/Type/TypeBase.h
@@ -4,20 +4,22 @@
 #define WINCOPIES_MACROS_TYPE_BASE_H
 
 #include "../../Environment.h"
+#include "../../../PP/Conditional/If.hpp"
 
 #define CONST_EXPR DLLEXPORT constexpr
 
-#define DEFAULT_C_D_TOR(name, modifier, implementation, ...) modifier name() __VA_ARGS__ = implementation;
+#define DEFAULT_C_D_TOR(name, modifier, implementation, ...) modifier name() __VA_ARGS__ implementation;
 
 #define DEFAULT_CONSTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(name, , implementation, __VA_ARGS__)
 #define DEFAULT_DESTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(~name, virtual, implementation, __VA_ARGS__)
 
-#define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR(name, default)
-#define INTERFACE_DESTRUCTOR(name, ...) public: DEFAULT_DESTRUCTOR(name, 0, __VA_ARGS__)
+#define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR(name, = default)
+#define _INTERFACE_DESTRUCTOR(name, _override, ...) public: DEFAULT_DESTRUCTOR(name, IF(_override, override, = 0), __VA_ARGS__)
+#define INTERFACE_DESTRUCTOR(name, ...) _INTERFACE_DESTRUCTOR(name, 1, __VA_ARGS__)
 
 #define INTERFACE_C_D_TOR(name, ...) INTERFACE_CONSTRUCTOR(name) INTERFACE_DESTRUCTOR(name, __VA_ARGS__)
 
-#define SCOPED_INTERFACE_C_D_TOR(name, ...) { INTERFACE_C_D_TOR(name, __VA_ARGS__) };
+#define SCOPED_INTERFACE_C_D_TOR(name, ...) { INTERFACE_CONSTRUCTOR(name) _INTERFACE_DESTRUCTOR(name, 0, __VA_ARGS__) };
 
 #define _STATIC_CLASS_C_D_TOR(name, prefix, ...) SURROUND(DEFAULT_, prefix, STRUCTOR)(name, delete, __VA_ARGS__)
 
