01bda097a7b2a434e900d36bf5d8907cf535ae7a
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-26 14:57:58 +0200

    New macros

--- Source/Include/Util/PP/MISC.hpp
+++ Source/Include/Util/PP/MISC.hpp
@@ -22,14 +22,22 @@
 #define _SURROUND(x, y) CONCATENATE(x, y)
 
 #define PREFIX(prefix, value, ...) prefix##value
+#define PREFIX_CS(prefix, value, ...) COMMA PREFIX(prefix, value)
+#define PREFIX_CE(prefix, value, ...) PREFIX(prefix, value) COMMA
+
 #define SURROUND(prefix, value, ...) _SURROUND(CONCATENATE(prefix, value), __VA_ARGS__)
+#define SURROUND_CS(prefix, value, ...) COMMA SURROUND(prefix, value, __VA_ARGS__)
+#define SURROUND_CE(prefix, value, ...) SURROUND(prefix, value, __VA_ARGS__) COMMA
+
 #define SUFFIX(prefix, value, ...) value##__VA_ARGS__
+#define SUFFIX_CS(prefix, value, ...) COMMA SUFFIX(, value, __VA_ARGS__)
+#define SUFFIX_CE(prefix, value, ...) SUFFIX(, value, __VA_ARGS__) COMMA
 
 #define ARGS_TRANSCRIPTION(value, ...) value __VA_ARGS__
 
 #define TRANSCRIBE_ARGS(prefix, value, ...) prefix value __VA_ARGS__
-#define TRANSCRIBE_ARGS_CS(prefix, value, ...) TRANSCRIBE_ARGS(COMMA prefix, value, __VA_ARGS__)
-#define TRANSCRIBE_ARGS_CE(prefix, value, ...) TRANSCRIBE_ARGS(prefix, value, __VA_ARGS__ COMMA)
+#define TRANSCRIBE_ARGS_CS(prefix, value, ...) COMMA TRANSCRIBE_ARGS(prefix, value, __VA_ARGS__)
+#define TRANSCRIBE_ARGS_CE(prefix, value, ...) TRANSCRIBE_ARGS(prefix, value, __VA_ARGS__) COMMA
 
 #define TRANSCRIBE_VA_ARGS(prefix, suffix, ...) prefix, __VA_ARGS__, suffix
 #endif // WINCOPIES_MISC_HPP
