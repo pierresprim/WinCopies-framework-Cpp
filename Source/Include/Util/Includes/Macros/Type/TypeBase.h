9a6b9862fec6cd5bd2e6b75a7842e66ce7e41747
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-28 20:21:22 +0200

    Add new macros

--- Source/Include/Util/Includes/Macros/Type/TypeBase.h
+++ Source/Include/Util/Includes/Macros/Type/TypeBase.h
@@ -4,9 +4,20 @@
 #define WINCOPIES_MACROS_TYPE_BASE_H
 
 #include "../../Environment.h"
-#include "../../../PP/Conditional/If.hpp"
+#include "../../../PP/Variadic/UtilBase.hpp"
 
 #define CONST_EXPR DLLEXPORT constexpr
+
+#define GET_TYPE_FULL_NAME(type, ...) IF_NO_VA_ARG(type, JOIN_ARGS(::, __VA_ARGS__, type), __VA_ARGS__)
+#define __GET_TYPE_FULL_NAME(type, ...) __VA_ARGS__ type
+#define _GET_TYPE_FULL_NAME(type, ...) IF_GREATER(COUNT_ARGS(__VA_ARGS__), 1, GET_TYPE_FULL_NAME, __GET_TYPE_FULL_NAME)(type, __VA_ARGS__)
+
+#define _ABSTRACT_METHOD_IMPLEMENTATION(type, method, ...) type::method() __VA_ARGS__ { /* Left empty. */ }
+#define ABSTRACT_METHOD_IMPLEMENTATION(isConst, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(GET_TYPE_FULL_NAME(type, __VA_ARGS__), method, IF(isConst, const))
+
+#define ___ABSTRACT_METHOD_IMPLEMENTATIONS(expand, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(IF(expand, FIRST_ARG method) type, IF(expand, SECOND_ARG) method, __VA_ARGS__)
+#define __ABSTRACT_METHOD_IMPLEMENTATIONS(type, method, ...) ___ABSTRACT_METHOD_IMPLEMENTATIONS(FIRST_ARG type, SECOND_ARG type, method, __VA_ARGS__)
+#define ABSTRACT_METHOD_IMPLEMENTATIONS(isConst, expand, type, methods, ...) FOR_EACH_C(__ABSTRACT_METHOD_IMPLEMENTATIONS, (expand, SURROUND(IF_NOT(expand, _), GET_TYPE_FULL_NAME)(type, __VA_ARGS__)), IF(isConst, const), EXPAND(methods))
 
 #define DEFAULT_C_D_TOR(name, modifier, implementation, ...) modifier name() __VA_ARGS__ implementation;
 
@@ -16,6 +27,8 @@
 #define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR(name, = default)
 #define _INTERFACE_DESTRUCTOR(name, _override, ...) public: DEFAULT_DESTRUCTOR(name, IF(_override, override, = 0), __VA_ARGS__)
 #define INTERFACE_DESTRUCTOR(name, ...) _INTERFACE_DESTRUCTOR(name, 1, __VA_ARGS__)
+
+#define ABSTRACT_DESTRUCTOR(name, ...) ABSTRACT_METHOD_IMPLEMENTATION(0, name, ~name, __VA_ARGS__)
 
 #define INTERFACE_C_D_TOR(name, ...) INTERFACE_CONSTRUCTOR(name) INTERFACE_DESTRUCTOR(name, __VA_ARGS__)
 
