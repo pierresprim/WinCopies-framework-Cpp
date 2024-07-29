6777bf5e587153b9b90e5ce982706ae8cd120549
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-29 23:46:05 +0200

    Update Delegates.h

--- Source/Include/Util/Delegates.h
+++ Source/Include/Util/Delegates.h
@@ -4,18 +4,19 @@
 #define WINCOPIES_DELEGATES_H
 
 #include "UtilBase.h"
-#include "Misc.h"
+#include "Typing.h"
 
 namespace WinCopies
 {
 	namespace Delegates
 	{
-		TEMPLATE_NC(2) DLLEXPORT INLINE_METHOD_RETURN(0, T2, Self, value, T1 value)
-		TEMPLATE using Self = Self<T, T>;
+		TEMPLATE INLINE_METHOD_RETURN(0, T, Self, value, T value)
 
-#define __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator) CONCATENATE(prefix, ACTION)(0, methodName, _operator, T& value) \
-CONCATENATE(prefix, RETURN)(0, T&, methodName, _operator, T& value)
-#define _WINCOPIES_DELEGATES_INC_DEC(prefix, _operator) __WINCOPIES_DELEGATES_INC_DEC(TEMPLATE DLLEXPORT inline INLINE_METHOD_, CONCATENATE(prefix, crement), SURROUND(_operator, _operator, value))
+#define ___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, modifier) CONCATENATE(prefix, ACTION)(0, methodName, _operator, T##modifier value)
+#define __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator) ___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, *) \
+___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, &) \
+CONCATENATE(prefix, RETURN)(0, T, methodName, _operator, T value)
+#define _WINCOPIES_DELEGATES_INC_DEC(prefix, _operator) __WINCOPIES_DELEGATES_INC_DEC(TEMPLATE DLLEXPORT INLINE_METHOD_, CONCATENATE(prefix, crement), SURROUND(_operator, _operator, value))
 
 			_WINCOPIES_DELEGATES_INC_DEC(In, +)
 			_WINCOPIES_DELEGATES_INC_DEC(De, -)
