e8d9d226a51e8e191dc5b7d4371b3ba119286d68
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-29 23:07:26 +0200

    Add new macros and rename TRANSCRIBE_ARGS

--- Source/Include/Util/PP/MISC.hpp
+++ Source/Include/Util/PP/MISC.hpp
@@ -24,20 +24,25 @@
 #define PREFIX(prefix, value, ...) prefix##value
 #define PREFIX_CS(prefix, value, ...) COMMA PREFIX(prefix, value)
 #define PREFIX_CE(prefix, value, ...) PREFIX(prefix, value) COMMA
+#define PREFIX_SPACED(prefix, value, ...) prefix value
+#define PREFIX_WS(prefix, value, ...) EMPTY PREFIX(prefix, value, ...)
 
 #define SURROUND(prefix, value, ...) _SURROUND(CONCATENATE(prefix, value), __VA_ARGS__)
 #define SURROUND_CS(prefix, value, ...) COMMA SURROUND(prefix, value, __VA_ARGS__)
 #define SURROUND_CE(prefix, value, ...) SURROUND(prefix, value, __VA_ARGS__) COMMA
+#define SURROUND_SPACED(prefix, value, ...) prefix value __VA_ARGS__
+#define SURROUND_WS(prefix, value, ...) EMPTY SURROUND(prefix, value, __VA_ARGS__)
 
 #define SUFFIX(prefix, value, ...) value##__VA_ARGS__
 #define SUFFIX_CS(prefix, value, ...) COMMA SUFFIX(, value, __VA_ARGS__)
 #define SUFFIX_CE(prefix, value, ...) SUFFIX(, value, __VA_ARGS__) COMMA
+#define SUFFIX_SPACED(prefix, value, ...) value __VA_ARGS__
+#define SUFFIX_WS(prefix, value, ...) EMPTY SUFFIX(prefix, value, ...)
 
 #define ARGS_TRANSCRIPTION(value, ...) value __VA_ARGS__
 
-#define TRANSCRIBE_ARGS(prefix, value, ...) prefix value __VA_ARGS__
-#define TRANSCRIBE_ARGS_CS(prefix, value, ...) COMMA TRANSCRIBE_ARGS(prefix, value, __VA_ARGS__)
-#define TRANSCRIBE_ARGS_CE(prefix, value, ...) TRANSCRIBE_ARGS(prefix, value, __VA_ARGS__) COMMA
+#define TRANSCRIBE_ARGS_CS(prefix, value, ...) COMMA SURROUND_SPACED(prefix, value, __VA_ARGS__)
+#define TRANSCRIBE_ARGS_CE(prefix, value, ...) SURROUND_SPACED(prefix, value, __VA_ARGS__) COMMA
 
 #define TRANSCRIBE_VA_ARGS(prefix, suffix, ...) prefix, __VA_ARGS__, suffix
 #endif WINCOPIES_MISC_HPP
