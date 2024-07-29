1bc62dc3907950db21d7abe08f5d21f5039b43a2
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-29 23:45:36 +0200

    Update Typing.h

--- Source/Include/Util/Typing.h
+++ Source/Include/Util/Typing.h
@@ -5,19 +5,18 @@
 
 #include "Includes/Macros/Template.h"
 #include "Includes/Typing/EnableIf.h"
+#include "Includes/Macros/Method/Inline.h"
 
 namespace WinCopies
 {
 	namespace Typing
 	{
-		TEMPLATE_NC(2, ENABLE_IF_BASE_OF(T1, T2) = true) inline T1 AsFromType(T2 value)
-		{
-			return value;
-		}
-		TEMPLATE_NC(2, ENABLE_IF_BASE_OF(T1, T2) = true) inline T1* AsFromType(T2* value)
-		{
-			return value;
-		}
+#define WINCOPIES_TYPING_AS_FROM_TYPE(modifier) TEMPLATE_NC(2, ENABLE_IF_BASE_OF(T1, T2) = true) INLINE_METHOD_RETURN(0, T1##modifier, AsFromType, value, T2##modifier value)
+
+		WINCOPIES_TYPING_AS_FROM_TYPE()
+		WINCOPIES_TYPING_AS_FROM_TYPE(*)
+		WINCOPIES_TYPING_AS_FROM_TYPE(&)
+#undef WINCOPIES_TYPING_AS_FROM_TYPE
 	}
 }
 #endif WINCOPIES_UTIL_TYPING_H
