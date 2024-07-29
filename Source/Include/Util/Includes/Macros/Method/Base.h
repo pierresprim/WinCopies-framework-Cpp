3abba692c21e4daf738362b9f264040ad67c2a81
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-07-29 23:15:31 +0200

    Add new macro

--- Source/Include/Util/Includes/Macros/Method/Base.h
+++ Source/Include/Util/Includes/Macros/Method/Base.h
@@ -3,11 +3,16 @@
 #ifndef WINCOPIES_MACROS_METHOD_BASE_H
 #define WINCOPIES_MACROS_METHOD_BASE_H
 
+#include "../../../PP/Variadic/UtilBase.hpp"
+#include "../../Environment.h"
+
 #define _METHOD(modifiers) DLLEXPORT modifiers void
 
 #define METHOD _METHOD()
 
 #define INLINE_METHOD _METHOD(inline)
 #define INLINE_FUNCTION DLLEXPORT inline
+
+#define RENDER_METHODS(type, renderer, ...) FOR_EACH(SURROUND_SPACED, , renderer, ;, TRANSCRIBE_PREFIXED(type, __VA_ARGS__))
 
 #endif WINCOPIES_MACROS_METHOD_BASE_H
