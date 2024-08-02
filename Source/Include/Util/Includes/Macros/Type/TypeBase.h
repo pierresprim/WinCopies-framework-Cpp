bf6276e552b3d2caf582f8806fd2b426dbbf6730
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-02 22:29:48 +0200

    Misc updates

--- Source/Include/Util/Includes/Macros/Type/TypeBase.h
+++ Source/Include/Util/Includes/Macros/Type/TypeBase.h
@@ -10,11 +10,14 @@
 
 #define DEFAULT_C_D_TOR(name, modifier, implementation, ...) modifier name() __VA_ARGS__ implementation;
 
+#define CONSTRUCTOR(name, ...) name::name(__VA_ARGS__)
+#define DESTRUCTOR(name) name::~name()
+
 #define DEFAULT_CONSTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(name, , implementation, __VA_ARGS__)
 #define DEFAULT_DESTRUCTOR(name, implementation, ...) DEFAULT_C_D_TOR(~name, virtual, implementation, __VA_ARGS__)
 
 #define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR(name, = default)
-#define _INTERFACE_DESTRUCTOR(name, _override, ...) public: DEFAULT_DESTRUCTOR(name, IF(_override, override, = 0), __VA_ARGS__)
+#define _INTERFACE_DESTRUCTOR(name, _override, ...) public: DEFAULT_DESTRUCTOR(name, IF(_override, override) = 0, __VA_ARGS__)
 #define INTERFACE_DESTRUCTOR(name, ...) _INTERFACE_DESTRUCTOR(name, 1, __VA_ARGS__)
 
 #define ABSTRACT_DESTRUCTOR(name, ...) ABSTRACT_METHOD_IMPLEMENTATION(0, name, ~name, __VA_ARGS__)
