086f2335c28691cb7fefbff81c62858231ec4da4
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-02 23:23:58 +0200

    Move some usings to Typing.h

--- Source/Include/Util/Typing.h
+++ Source/Include/Util/Typing.h
@@ -3,6 +3,9 @@
 #ifndef WINCOPIES_UTIL_TYPING_H
 #define WINCOPIES_UTIL_TYPING_H
 
+#include <memory>
+#include <type_traits>
+
 #include "Includes/Macros/Template.h"
 #include "Includes/Typing/EnableIf.h"
 #include "Includes/Macros/Method/Inline.h"
@@ -11,6 +14,9 @@ namespace WinCopies
 {
 	namespace Typing
 	{
+		TEMPLATE using FreeableUniquePtr = unique_ptr<T, void (*)(void*)>;
+		TEMPLATE using UnderlyingType = underlying_type_t<T>;
+		
 #define WINCOPIES_TYPING_AS_FROM_TYPE(modifier) TEMPLATE_NC(2, ENABLE_IF_BASE_OF(T1, T2) = true) INLINE_METHOD_RETURN(0, T1##modifier, AsFromType, value, T2##modifier value)
 
 		WINCOPIES_TYPING_AS_FROM_TYPE()
