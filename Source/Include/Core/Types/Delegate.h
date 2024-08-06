31f777d5995cd37315b85f3a74291ce4b76a1a68
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-06 23:06:15 +0200

    Move templates to new file

---
+++ Source/Include/Core/Types/Delegate.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef WINCOPIES_TYPES_DELEGATE_H
+#define WINCOPIES_TYPES_DELEGATE_H
+
+#include "../../Kernel/Macros/Template.hxx"
+#include "../../Kernel/Types/Delegates/Delegates.hxx"
+
+namespace WinCopies
+{
+	namespace Delegate
+	{
+		TEMPLATE using Function = T(*)();
+		TEMPLATE using FunctionFunction = std::function<T()>;
+
+		TEMPLATE_EC(TObj) using InstanceActionVoid = INSTANCE_DELEGATE(TObj, void);
+		TEMPLATE_EC(TObj, T) using InstanceFunction = INSTANCE_DELEGATE(TObj, T);
+		
+		CREATE_DELEGATE_ACTION(0, _DELEGATE_ACTION)
+		CREATE_DELEGATE_ACTION(1, _INSTANCE_ACTION)
+		CREATE_DELEGATE_ACTION(0, _STD_ACTION)
+
+		CREATE_DELEGATE_ACTIONS(DELEGATE)
+		CREATE_DELEGATE_ACTIONS(INSTANCE)
+		CREATE_DELEGATE_ACTIONS(ACTION)
+
+		CREATE_SELECTOR_DELEGATES(Converter, T)
+		CREATE_SELECTOR_DELEGATES(Predicate, bool)
+
+		CREATE_SELECTORS(_PREDICATE)
+		CREATE_SELECTORS(_COMPARISON)
+		CREATE_SELECTORS(_EQUALITY_COMPARISON)
+
+		CREATE_COMPARISON_DELEGATES()
+		CREATE_COMPARISON_DELEGATES(Equality)
+	}
+}
+#endif WINCOPIES_TYPES_DELEGATE_H
--- Source/Include/Util/Delegate.h
+++ Source/Include/Util/Delegate.h
@@ -3,6 +3,8 @@
 #ifndef WINCOPIES_DELEGATE_H
 #define WINCOPIES_DELEGATE_H
 
+#include "../Core/Types/Delegate.h"
+
 #include "UtilBase.h"
 
 namespace WinCopies
@@ -11,30 +13,6 @@ namespace WinCopies
 	{
 		typedef void(*ActionVoid)();
 		typedef std::function<void()> ActionFunctionVoid;
-
-		TEMPLATE using Function = T(*)();
-		TEMPLATE using FunctionFunction = std::function<T()>;
-
-		TEMPLATE_EC(TObj) using InstanceActionVoid = INSTANCE_DELEGATE(TObj, void);
-		TEMPLATE_EC(TObj, T) using InstanceFunction = INSTANCE_DELEGATE(TObj, T);
-		
-		CREATE_DELEGATE_ACTION(0, _DELEGATE_ACTION)
-		CREATE_DELEGATE_ACTION(1, _INSTANCE_ACTION)
-		CREATE_DELEGATE_ACTION(0, _STD_ACTION)
-
-		CREATE_DELEGATE_ACTIONS(DELEGATE)
-		CREATE_DELEGATE_ACTIONS(INSTANCE)
-		CREATE_DELEGATE_ACTIONS(ACTION)
-
-		CREATE_SELECTOR_DELEGATES(Converter, T)
-		CREATE_SELECTOR_DELEGATES(Predicate, bool)
-
-		CREATE_SELECTORS(_PREDICATE)
-		CREATE_SELECTORS(_COMPARISON)
-		CREATE_SELECTORS(_EQUALITY_COMPARISON)
-
-		CREATE_COMPARISON_DELEGATES()
-		CREATE_COMPARISON_DELEGATES(Equality)
 	}
 }
 #endif WINCOPIES_DELEGATE_H
