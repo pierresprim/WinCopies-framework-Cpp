0d180c876ef71a571a1236d0cd51b03d4f10c270
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-06 23:04:31 +0200

    Update includes

--- Source/Include/Core/Enum/Operators.h
+++ Source/Include/Core/Enum/Operators.h
@@ -4,6 +4,9 @@
 #define WINCOPIES_UTIL_ENUM_OPERATORS_HPP
 
 #include "../../Kernel/Enum/Operators.hxx"
+#include "../../Kernel/Typing/Enum.hxx"
+
+#include "../Typing/EnableIf.h"
 
 ENUM_COMPARISON_OPERATOR(== )
 ENUM_COMPARISON_OPERATOR2(== )
