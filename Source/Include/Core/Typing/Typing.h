428cd6f975b22ee852fcfe1141a652a7c575b42c
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-07 22:07:50 +0200

    Move types in Typing.h to subnamespaces

--- Source/Include/Core/Typing/Typing.h
+++ Source/Include/Core/Typing/Typing.h
@@ -8,10 +8,21 @@
 #include "../Types/Delegate.h"
 
 using namespace ::std;
+using namespace ::WinCopies;
 
-TEMPLATE using FreeableUniquePtr = unique_ptr<T, void (*)(void*)>;
-TEMPLATE using UnderlyingType = underlying_type_t<T>;
-TEMPLATE using DualPredicate = PredicateFunction2<T, ErrorCode>;
-TEMPLATE using SystemDualPredicate = PredicateFunction2<T, SystemErrorCode>;
+namespace WinCopies
+{
+	namespace Typing
+	{
+		TEMPLATE using FreeableUniquePtr = unique_ptr<T, void (*)(void*)>;
+		TEMPLATE using UnderlyingType = underlying_type_t<T>;
+	}
+
+	namespace Delegate
+	{
+		TEMPLATE using DualPredicate = PredicateFunction2<T, ErrorCode>;
+		TEMPLATE using SystemDualPredicate = PredicateFunction2<T, SystemErrorCode>;
+	}
+}
 
 #endif WINCOPIES_TYPING_TYPING_H
