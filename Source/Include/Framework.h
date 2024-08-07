614051bc816a875c22aaa0d95dd91204fe6afea0
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-07 21:57:38 +0200

    Add new header file

---
+++ Source/Include/Framework.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef WINCOPIES_FRAMEWORK_H
+#define WINCOPIES_FRAMEWORK_H
+
+#include "Kernel/Framework.h"
+
+#include "Core/Enums/ErrorCode.h"
+#include "Core/Enums/SystemErrorCode.h"
+#include "Core/Enum/Bitwise.h"
+#include "Core/Typing/EnableIf.h"
+
+#endif WINCOPIES_FRAMEWORK_H
