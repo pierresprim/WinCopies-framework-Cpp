6be0b745ea9c2d4fce4e59d1a4dd36658c35d62e
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-09 22:58:28 +0200

    Update Nullable.h

--- Source/Include/Util/Nullable.h
+++ Source/Include/Util/Nullable.h
@@ -19,12 +19,12 @@ namespace WinCopies
 		Nullable() = default;
 		INLINE_FIELD_SET(Nullable, T& const, value)
 
-		IINLINE_FIELD_RETURN(bool, HasValue, hasValue)
+		IINLINE_FIELD_RETURN(VIRTUALITY_NONE, bool, HasValue, hasValue)
 
 		GET_FIELD_IF_ELSE(ErrorCode, TryGetValue, T, value, _hasValue, ErrorCode::Success, default(T), ErrorCode::EmptyObject)
 
-		INLINE_METHOD_RETURN(1, T, GetValueOrDefault, _hasValue ? _value : default(T))
-		INLINE_METHOD_RETURN(1, T, GetValueOrCustom, _hasValue ? _value : custom, T custom)
+		INLINE_METHOD_RETURN(1, VIRTUALITY_NONE, T, GetValueOrDefault, _hasValue ? _value : default(T))
+		INLINE_METHOD_RETURN(1, VIRTUALITY_NONE, T, GetValueOrCustom, _hasValue ? _value : custom, T custom)
 	};
 }
 
