429d6a0774b85cf3bc3784ec57b8e5c12cc202a5
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-09 23:04:04 +0200

    Update Typing.h

--- Source/Include/Util/Typing.h
+++ Source/Include/Util/Typing.h
@@ -21,15 +21,15 @@ namespace WinCopies
 {
 	namespace Typing
 	{
-#define WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(tOut) TEMPLATE_NC(2, IF_NOT(tOut, , class TOut COMMA) ENABLE_IF_BASE_OF(T1, T2) = true)
-#define WINCOPIES_TYPING_AS_FROM_TYPE(modifier) WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(0) INLINE_METHOD_RETURN(0, VIRTUALITY_NONE, T1##modifier, AsFromType, value, T2##modifier value)
+#define WINCOPIES_AS_FROM_TYPE_HEADER(tOut) TEMPLATE_NC(2, IF_NOT(tOut, , class TOut COMMA) ENABLE_IF_BASE_OF(T1, T2) = true)
+#define WINCOPIES_AS_FROM_TYPE(modifier) WINCOPIES_AS_FROM_TYPE_HEADER(0) INLINE_METHOD_RETURN(0, VIRTUALITY_NONE, T1##modifier, AsFromType, value, T2##modifier value)
 
-		WINCOPIES_TYPING_AS_FROM_TYPE()
-		WINCOPIES_TYPING_AS_FROM_TYPE(*)
-		WINCOPIES_TYPING_AS_FROM_TYPE(&)
-#undef WINCOPIES_TYPING_AS_FROM_TYPE
+		WINCOPIES_AS_FROM_TYPE()
+		WINCOPIES_AS_FROM_TYPE(*)
+		WINCOPIES_AS_FROM_TYPE(&)
+#undef WINCOPIES_AS_FROM_TYPE
 
-		WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, T2** _result, SelectorFunction<T2**, TOut> func)
+		WINCOPIES_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, T2** _result, SelectorFunction<T2**, TOut> func)
 		{
 			TOut __result = func(_result);
 
@@ -37,13 +37,13 @@ namespace WinCopies
 
 			return __result;
 		}
-		WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, SelectorFunction<T2**, TOut> func)
+		WINCOPIES_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, SelectorFunction<T2**, TOut> func)
 		{
 			T2* ptr = 0;
 
 			return AsFromType(result, &ptr, func);
 		}
-#undef WINCOPIES_TYPING_AS_FROM_TYPE_HEADER
+#undef WINCOPIES_AS_FROM_TYPE_HEADER
 	}
 }
 #endif WINCOPIES_UTIL_TYPING_H
