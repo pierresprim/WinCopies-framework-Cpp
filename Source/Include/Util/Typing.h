1a6a8fecbc7167454b7b47248d178aba51d644f6
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-09 23:02:02 +0200

    Update Typing.h

--- Source/Include/Util/Typing.h
+++ Source/Include/Util/Typing.h
@@ -11,6 +11,8 @@
 
 #include "../Core/Typing/EnableIf.h"
 
+#include "../Core/Typing/Typing.h"
+
 #include "Delegate.h"
 
 using namespace WinCopies::Delegate;
@@ -19,12 +21,14 @@ namespace WinCopies
 {
 	namespace Typing
 	{
-#define WINCOPIES_TYPING_AS_FROM_TYPE(modifier) TEMPLATE_NC(2, ENABLE_IF_BASE_OF(T1, T2) = true) INLINE_METHOD_RETURN(0, T1##modifier, AsFromType, value, T2##modifier value)
+#define WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(tOut) TEMPLATE_NC(2, IF_NOT(tOut, , class TOut COMMA) ENABLE_IF_BASE_OF(T1, T2) = true)
+#define WINCOPIES_TYPING_AS_FROM_TYPE(modifier) WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(0) INLINE_METHOD_RETURN(0, VIRTUALITY_NONE, T1##modifier, AsFromType, value, T2##modifier value)
 
 		WINCOPIES_TYPING_AS_FROM_TYPE()
 		WINCOPIES_TYPING_AS_FROM_TYPE(*)
 		WINCOPIES_TYPING_AS_FROM_TYPE(&)
 #undef WINCOPIES_TYPING_AS_FROM_TYPE
+#undef WINCOPIES_TYPING_AS_FROM_TYPE_HEADER
 	}
 }
 #endif WINCOPIES_UTIL_TYPING_H
