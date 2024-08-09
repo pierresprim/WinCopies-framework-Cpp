29c52b5aea3db625ae0546be5f6769b2b2a93600
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-09 23:02:27 +0200

    Add functions

--- Source/Include/Util/Typing.h
+++ Source/Include/Util/Typing.h
@@ -28,6 +28,21 @@ namespace WinCopies
 		WINCOPIES_TYPING_AS_FROM_TYPE(*)
 		WINCOPIES_TYPING_AS_FROM_TYPE(&)
 #undef WINCOPIES_TYPING_AS_FROM_TYPE
+
+		WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, T2** _result, SelectorFunction<T2**, TOut> func)
+		{
+			TOut __result = func(_result);
+
+			*result = *_result;
+
+			return __result;
+		}
+		WINCOPIES_TYPING_AS_FROM_TYPE_HEADER(1) TOut AsFromType(T1** result, SelectorFunction<T2**, TOut> func)
+		{
+			T2* ptr = 0;
+
+			return AsFromType(result, &ptr, func);
+		}
 #undef WINCOPIES_TYPING_AS_FROM_TYPE_HEADER
 	}
 }
