301dd3e2e3ee06358ca022db931c5cfa3727e6bb
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-10 20:39:33 +0200

    Misc updates

--- Source/Include/Util/Delegates.h
+++ Source/Include/Util/Delegates.h
@@ -10,16 +10,19 @@ namespace WinCopies
 {
 	namespace Delegates
 	{
-		TEMPLATE INLINE_METHOD_RETURN(0, T, Self, value, T value)
+		TEMPLATE INLINE_METHOD_RETURN(0, VIRTUALITY_NONE, T, Self, value, T value)
 
-#define ___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, modifier) CONCATENATE(prefix, ACTION)(0, methodName, _operator, T##modifier value)
-#define __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator) ___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, *) \
-___WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, &) \
-CONCATENATE(prefix, RETURN)(0, T, methodName, _operator, T value)
-#define _WINCOPIES_DELEGATES_INC_DEC(prefix, _operator) __WINCOPIES_DELEGATES_INC_DEC(TEMPLATE DLLEXPORT INLINE_METHOD_, CONCATENATE(prefix, crement), SURROUND(_operator, _operator, value))
+#define __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, modifier) CONCATENATE(prefix, ACTION)(0, methodName, _operator, T##modifier value)
+#define _WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator) __WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, *) \
+	__WINCOPIES_DELEGATES_INC_DEC(prefix, methodName, _operator, &) \
+	CONCATENATE(prefix, RETURN)(0, VIRTUALITY_NONE, T, methodName, _operator, T value)
+#define WINCOPIES_DELEGATES_INC_DEC(prefix, _operator) _WINCOPIES_DELEGATES_INC_DEC(TEMPLATE DLLEXPORT INLINE_METHOD_, CONCATENATE(prefix, crement), SURROUND(_operator, _operator, value))
 
-			_WINCOPIES_DELEGATES_INC_DEC(In, +)
-			_WINCOPIES_DELEGATES_INC_DEC(De, -)
+			WINCOPIES_DELEGATES_INC_DEC(In, +)
+			WINCOPIES_DELEGATES_INC_DEC(De, -)
+#undef WINCOPIES_DELEGATES_INC_DEC
+#undef _WINCOPIES_DELEGATES_INC_DEC
+#undef __WINCOPIES_DELEGATES_INC_DEC
 	}
 }
 
