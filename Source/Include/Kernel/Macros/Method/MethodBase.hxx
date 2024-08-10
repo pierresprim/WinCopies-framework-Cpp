30e5b644fdd95ace7b48d556d5355c6c1f23ccb7
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-10 20:29:11 +0200

    Rename ZIP_PARAMETERS macros

--- Source/Include/Kernel/Macros/Method/MethodBase.hxx
+++ Source/Include/Kernel/Macros/Method/MethodBase.hxx
@@ -5,7 +5,7 @@
 
 #include "../../../PP/Variadic/UtilBase.hpp"
 
-#define _ZIP_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) ZIP_ARGS((SURROUND_ARGS(FIRST_ARG ptrType _namespace FIRST_ARG, genericTypeArgument SECOND_ARG ptrType, __VA_ARGS__)), PREFIX_ARGS(SECOND_ARG, __VA_ARGS__))
-#define ZIP_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) _ZIP_PARAMETERS(IF_VA_ARGS((ptrType <, >), , ptrType), _namespace, IF_VA_ARGS(<genericTypeArgument>, , genericTypeArgument), __VA_ARGS__)
+#define _MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) MAKE_ARGS((SURROUND_ARGS(FIRST_ARG ptrType _namespace FIRST_ARG, genericTypeArgument SECOND_ARG ptrType, __VA_ARGS__)), PREFIX_ARGS(SECOND_ARG, __VA_ARGS__))
+#define MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) _MAKE_PARAMETERS(IF_VA_ARGS((ptrType <, >), , ptrType), _namespace, IF_VA_ARGS(<genericTypeArgument>, , genericTypeArgument), __VA_ARGS__)
 
 #endif WINCOPIES_MACROS_METHOD_METHOD_BASE_HXX
