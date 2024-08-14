dc01e87057c377d66e35ad5f1f29c05696fcf635
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-14 06:08:47 +0200

    Update MAKE_PARAMETERS macros

--- Source/Include/Kernel/Macros/Method/MethodBase.hxx
+++ Source/Include/Kernel/Macros/Method/MethodBase.hxx
@@ -5,14 +5,19 @@
 
 #include "../../../PP/Variadic/UtilBase.hpp"
 
-#define __MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) ZIP_ARGS((SURROUND_ARGS(FIRST_ARG ptrType _namespace FIRST_ARG, genericTypeArgument SECOND_ARG ptrType, __VA_ARGS__)), PREFIX_ARGS(SECOND_ARG, __VA_ARGS__))
+#define _MAKE_PARAMETERS_SPACED(prefix, value, ...) ((prefix FIRST_ARG value) EXPAND(__VA_ARGS__))
+#define _MAKE_PARAMETERS_CS(prefix, value, ...) COMMA _MAKE_PARAMETERS_SPACED(prefix, value, __VA_ARGS__)
 
-#define __MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArgument, ...) (__MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, __VA_ARGS__)),
-#define _MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArgument, ...) IF_VA_ARGS(__MAKE_PARAMETER_ARRAY, DISCARD, __VA_ARGS__)(ptrType, _namespace, genericTypeArgument, __VA_ARGS__)
+#define ____MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, renderer, ...) ZIP_ARGS((__SURROUND_ARGS(FIRST_ARG ptrType _namespace renderer, (EXPAND(genericTypeArguments) SECOND_ARG ptrType), _MAKE_PARAMETERS_SPACED, _MAKE_PARAMETERS_CS, __VA_ARGS__)), PREFIX_ARGS(SECOND_ARG, __VA_ARGS__))
+#define ___MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, ...) ____MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, IF(VA_ARGS_EMPTY(EXPAND(genericTypeArguments)), SINGLE_ARG), __VA_ARGS__)
 
-#define _MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, _array, ...) _MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArgument, EXPAND(_array)) __MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, __VA_ARGS__)
+#define __MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArguments, ...) (___MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, __VA_ARGS__)),
+#define _MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArguments, ...) CALL_IF_VA_ARGS(__MAKE_PARAMETER_ARRAY, __VA_ARGS__)(ptrType, _namespace, genericTypeArguments, __VA_ARGS__)
 
-#define MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, _array, ...) _MAKE_PARAMETERS(IF_VA_ARGS((ptrType <, >), , ptrType), _namespace, IF_VA_ARGS(<genericTypeArgument>, , genericTypeArgument), _array, __VA_ARGS__)
-#define MAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, ...) MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArgument, (), __VA_ARGS__)
+#define __MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, _array, ...) _MAKE_PARAMETER_ARRAY(ptrType, _namespace, genericTypeArguments, EXPAND(_array)) ___MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, __VA_ARGS__)
+#define _MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, _array, ...) __MAKE_PARAMETERS(ptrType, SUFFIX_VA_ARGS(::, _namespace), genericTypeArguments, _array, __VA_ARGS__)
+
+#define MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, _array, ...) _MAKE_PARAMETERS(IF_VA_ARGS((ptrType <, >), , ptrType), _namespace, (GET_TEMPLATE_ARGS(EXPAND(genericTypeArguments))), _array, __VA_ARGS__)
+#define MAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, ...) MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, (), __VA_ARGS__)
 
 #endif WINCOPIES_MACROS_METHOD_METHOD_BASE_HXX
