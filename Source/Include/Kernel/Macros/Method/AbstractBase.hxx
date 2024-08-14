9752d3f0a68f4d3cc7fb60a754d4535b950ec5c6
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-14 06:06:16 +0200

    New macros

--- Source/Include/Kernel/Macros/Method/AbstractBase.hxx
+++ Source/Include/Kernel/Macros/Method/AbstractBase.hxx
@@ -5,11 +5,12 @@
 
 #include "../../../PP/Variadic/UtilBase.hpp"
 
-#if !HAS_VA_OPT
-#include "../../../PP/Variadic/Variadic.hpp"
-#endif
+#include "../Type/TypeBase.inc.hxx"
 
-#define _ABSTRACT_METHOD_IMPLEMENTATION(type, method, ...) type::method() __VA_ARGS__ { /* Left empty. */ }
-#define ABSTRACT_METHOD_IMPLEMENTATION(isConst, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(GET_TYPE_FULL_NAME(type, __VA_ARGS__), method, IF(isConst, const))
+#define __ABSTRACT_METHOD_IMPLEMENTATION(type, method, ...) type::method() __VA_ARGS__ { /* Left empty. */ }
+#define _ABSTRACT_METHOD_IMPLEMENTATION(isConst, templateArgs, type, method, ...) __ABSTRACT_METHOD_IMPLEMENTATION(GET_TYPE_FULL_NAME(type, __VA_ARGS__) GET_TEMPLATE_ARGS(EXPAND(templateArgs)), method, IF(isConst, const))
+
+#define ABSTRACT_METHOD_IMPLEMENTATION(isConst, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(0, (), type, method, __VA_ARGS__)
+#define ABSTRACT_TEMPLATE_METHOD_IMPLEMENTATION(isConst, templateArgs, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(0, templateArgs, type, method, __VA_ARGS__)
 
 #endif WINCOPIES_MACROS_METHOD_ABSTRACT_BASE_HXX
