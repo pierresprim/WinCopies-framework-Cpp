da57fe2e13f63ca5c2a98b164d26d98669516343
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-14 05:52:53 +0200

    Update Util.hpp

--- Source/Include/PP/Variadic/Util.hpp
+++ Source/Include/PP/Variadic/Util.hpp
@@ -3,8 +3,11 @@
 #ifndef WINCOPIES_VA_UTIL_HPP
 #define WINCOPIES_VA_UTIL_HPP
 
-#include "../Conditional/Conditional.hpp"
 #include "UtilBase.hpp"
+
+#if HAS_VA_OPT
+#include "Variadic.hpp"
+#endif
 
 #define GET_LAST_ARG_INDEX(...) SUB_OR_DISCARD_IF_ZERO(COUNT_ARGS(__VA_ARGS__))
 
@@ -13,7 +16,7 @@
 #define ____GET_ARG_INDEX_FROM_LAST(i, n) IF_LESS(i, n, 0)
 #define ___GET_ARG_INDEX_FROM_LAST(i, j, k) IF(j, j, ____GET_ARG_INDEX_FROM_LAST(i, k))
 #define __GET_ARG_INDEX_FROM_LAST(i, n) ___GET_ARG_INDEX_FROM_LAST(i, SUB(n, INCREMENT(i)), n)
-#define _GET_ARG_INDEX_FROM_LAST(i, n) IF(n, __GET_ARG_INDEX_FROM_LAST, DISCARD)(i, n)
+#define _GET_ARG_INDEX_FROM_LAST(i, n) CALL_IF(n, __GET_ARG_INDEX_FROM_LAST)(i, n)
 #define GET_ARG_INDEX_FROM_LAST(i, ...) _GET_ARG_INDEX_FROM_LAST(i, COUNT_ARGS(__VA_ARGS__))
 
 #define GET_ARG_FROM_LAST(i, ...) GET_ARG(GET_ARG_INDEX_FROM_LAST(i, __VA_ARGS__), __VA_ARGS__)
