bfc8ff8e1ac7d3d22d30a0d9a763fb315a739d01
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-14 06:05:27 +0200

    New macros

--- Source/Include/Kernel/Macros/Type/TypeBase.inc.hxx
+++ Source/Include/Kernel/Macros/Type/TypeBase.inc.hxx
@@ -7,4 +7,8 @@
 
 #define GET_TYPE_FULL_NAME(type, ...) IF_NO_VA_ARG(type, JOIN_ARGS(::, __VA_ARGS__, type), __VA_ARGS__)
 
+#define PREFIX_TEMPLATE_TYPES(...) PREFIX_ARGS(T, __VA_ARGS__)
+
+#define GET_TEMPLATE_ARGS(...) SURROUND_VA_ARGS(<, >, __VA_ARGS__)
+
 #endif WINCOPIES_MACROS_TYPE_BASE_INC_HXX
