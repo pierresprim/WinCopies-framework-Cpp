48e16bddbbca15de51af0184816a1604dab8f76d
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-21 20:29:02 +0200

    New delegates

--- Source/Include/Util/Delegate.h
+++ Source/Include/Util/Delegate.h
@@ -11,9 +11,18 @@ namespace WinCopies
 	{
 		typedef void(*ActionVoid)();
 		typedef std::function<void()> ActionFunctionVoid;
-		
-		typedef Function<bool> BoolFunc;
-		typedef FunctionFunction<bool> BoolFunction;
+
+#define CREATE_BOOL_FUNC(type, typeNamePrefix, typeNameSuffix, nameSuffix) typedef Function##typeNameSuffix<type> typeNamePrefix##Func##nameSuffix;
+#define __CREATE_BOOL_FUNCS(type, typeNamePrefix) CREATE_BOOL_FUNC(type, typeNamePrefix, , ) CREATE_BOOL_FUNC(type, typeNamePrefix, Function, tion)
+#define _CREATE_BOOL_FUNCS(type) __CREATE_BOOL_FUNCS(type, type)
+#define CREATE_BOOL_FUNCS(prefix) _CREATE_BOOL_FUNCS(prefix##ErrorCode)
+
+		__CREATE_BOOL_FUNCS(bool, Bool)
+
+		CREATE_BOOL_FUNCS()
+		CREATE_BOOL_FUNCS(System)
+
+#undef CREATE_BOOL_FUNCS
 	}
 }
 #endif WINCOPIES_DELEGATE_H
