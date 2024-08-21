4eab818ba90cca7513ce54b9c130c8e6f8879cce
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-08-21 19:59:29 +0200

    Update AbstractBase.hxx

--- Source/Include/Kernel/Macros/Method/AbstractBase.hxx
+++ Source/Include/Kernel/Macros/Method/AbstractBase.hxx
@@ -5,10 +5,7 @@
 
 #include "../Type/TypeBase.inc.hxx"
 
-#define __ABSTRACT_METHOD_IMPLEMENTATION(type, method, ...) type::method() __VA_ARGS__ { /* Left empty. */ }
-#define _ABSTRACT_METHOD_IMPLEMENTATION(isConst, templateArgs, type, method, ...) __ABSTRACT_METHOD_IMPLEMENTATION(GET_TYPE_FULL_NAME(type, __VA_ARGS__) GET_TEMPLATE_ARGS(EXPAND(templateArgs)), method, IF(isConst, const))
-
-#define ABSTRACT_METHOD_IMPLEMENTATION(isConst, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(0, (), type, method, __VA_ARGS__)
-#define ABSTRACT_TEMPLATE_METHOD_IMPLEMENTATION(isConst, templateArgs, type, method, ...) _ABSTRACT_METHOD_IMPLEMENTATION(0, templateArgs, type, method, __VA_ARGS__)
+#define ABSTRACT_TEMPLATE_METHOD_IMPLEMENTATION(isConst, templateArgs, type, method, ...) GET_TYPE_FULL_NAME(type, __VA_ARGS__)GET_TEMPLATE_ARGS(EXPAND(templateArgs))::method() IF(isConst, const) { /* Left empty. */ }
+#define ABSTRACT_METHOD_IMPLEMENTATION(isConst, type, method, ...) ABSTRACT_TEMPLATE_METHOD_IMPLEMENTATION(0, (), type, method, __VA_ARGS__)
 
 #endif WINCOPIES_MACROS_METHOD_ABSTRACT_BASE_HXX
