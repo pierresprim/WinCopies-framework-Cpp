855962e939f7c669c1943ff84019acd3ea9550e5
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-26 15:02:58 +0200

    New macros

--- Source/Include/Kernel/Macros/Method/Property.hxx
+++ Source/Include/Kernel/Macros/Method/Property.hxx
@@ -7,10 +7,18 @@
 
 #define _PROPERTY_GET(type) type, Get##type
 #define _PROPERTY_IS(name) bool, Is##name
+#define _PROPERTY_HAS(name) bool, Has##name
 
 #define MAKE_PROPERTY_DEFINITION(value, accessorType) PRINT_FIRST_TWO_ARGS(CONCATENATE(_PROPERTY_, accessorType)(value))
 
 #define PROPERTY_GET(type) MAKE_PROPERTY_DEFINITION(type, GET)
 #define PROPERTY_IS(name) MAKE_PROPERTY_DEFINITION(name, IS)
+#define PROPERTY_HAS(name) MAKE_PROPERTY_DEFINITION(name, HAS)
+
+#define _PROPERTIES(propertyKind, renderer, ...) PRINT_RENDERED_SUFFIXED(; , renderer, RENDER_ARGS(PROPERTY_##propertyKind, __VA_ARGS__))
+
+#define PROPERTIES_GET(renderer, ...) _PROPERTIES(GET, renderer, __VA_ARGS__)
+#define PROPERTIES_IS(renderer, ...) _PROPERTIES(IS, renderer, __VA_ARGS__)
+#define PROPERTIES_HAS(renderer, ...) _PROPERTIES(HAS, renderer, __VA_ARGS__)
 
 #endif WINCOPIES_MACROS_METHOD_PROPERTY_HXX
