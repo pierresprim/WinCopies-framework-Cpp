d1d1e95c9d1625b4a27cc414aa1e4b60d8bbdb00
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-08-26 14:56:16 +0200

    Improve SHARED_TYPE/FROM_THIS

--- Source/Include/Kernel/Macros/Type/Shared.hxx
+++ Source/Include/Kernel/Macros/Type/Shared.hxx
@@ -5,8 +5,8 @@
 
 #include "../../../Core/Typing/Typing.h"
 
-#define SHARED_TYPE(type) ::WinCopies::Typing::SharedType<type>
-#define SHARED_FROM_THIS(type) public SHARED_TYPE(type)
+#define SHARED_TYPE(...) ::WinCopies::Typing::SharedType<__VA_ARGS__>
+#define SHARED_FROM_THIS(...) public SHARED_TYPE(__VA_ARGS__)
 #define SHARED_TYPE_HEADER(typeKind, typeName, ...) typeKind typeName : SHARED_FROM_THIS(typeName IF_TRUE(VA_ARGS_FILLED(__VA_ARGS__), <__VA_ARGS__>))
 
 #define SHARED_CLASS(className, ...) SHARED_TYPE_HEADER(CLASS, className, __VA_ARGS__)
