e89bba99a32709cf82c893f936dd4be837c994fe
pierresprim <49271108+pierresprim@users.noreply.github.com>
2024-09-04 02:29:11 +0200

    Update macros

--- Source/Include/Kernel/Macros/Type/TypeBase.hxx
+++ Source/Include/Kernel/Macros/Type/TypeBase.hxx
@@ -23,8 +23,8 @@
 #define DEFAULT_CONSTRUCTOR_DEFINITION(name) DEFAULT_CONSTRUCTOR(name, = default)
 
 #define INTERFACE_CONSTRUCTOR(name) protected: explicit DEFAULT_CONSTRUCTOR_DEFINITION(name)
-#define _INTERFACE_DESTRUCTOR(name, _override, ...) public: DEFAULT_DESTRUCTOR(name, IF(_override, override) = 0 { }, __VA_ARGS__)
-#define INTERFACE_DESTRUCTOR(name, ...) _INTERFACE_DESTRUCTOR(name, 1, __VA_ARGS__)
+#define _INTERFACE_DESTRUCTOR(name, _override) public: DEFAULT_DESTRUCTOR(name, IF(_override, override) = 0 { })
+#define INTERFACE_DESTRUCTOR(name) _INTERFACE_DESTRUCTOR(name, 1)
 
 #define _ABSTRACT_DESTRUCTOR(isTemplate, name, ...) ABSTRACT_METHOD_IMPLEMENTATION(0, name IF(isTemplate, <T>), ~name, __VA_ARGS__)
 
@@ -34,9 +34,9 @@
 #define ABSTRACT_TEMPLATE_DESTRUCTOR(name, ...) TEMPLATE _ABSTRACT_DESTRUCTOR(1, name, __VA_ARGS__)
 #define ABSTRACT_SELECTOR_DESTRUCTOR(name, ...) SELECTOR_TEMPLATE AABSTRACT_SELECTOR_DESTRUCTOR(name, __VA_ARGS__)
 
-#define INTERFACE_C_D_TOR(name, ...) INTERFACE_CONSTRUCTOR(name) INTERFACE_DESTRUCTOR(name, __VA_ARGS__)
+#define INTERFACE_C_D_TOR(name) INTERFACE_CONSTRUCTOR(name) INTERFACE_DESTRUCTOR(name)
 
-#define SCOPED_INTERFACE_C_D_TOR(name, ...) { INTERFACE_CONSTRUCTOR(name) _INTERFACE_DESTRUCTOR(name, 0, __VA_ARGS__) };
+#define SCOPED_INTERFACE_C_D_TOR(name, ...) { INTERFACE_CONSTRUCTOR(name) _INTERFACE_DESTRUCTOR(name, 0) };
 
 #define _STATIC_CLASS_C_D_TOR(name, prefix, ...) SURROUND(DEFAULT_, prefix, STRUCTOR)(name, delete, __VA_ARGS__)
 
@@ -51,7 +51,7 @@
 #define __TYPE_INITIALIZATION(name, baseType, base, ...) _FFIELDS_INITIALIZER(1, name, (), (), (baseType, EXPAND(base)), __VA_ARGS__)
 #define _TYPE_INITIALIZATION(name, baseType, ...) __TYPE_INITIALIZATION(name, baseType, FIRST_ARG(__VA_ARGS__), ALL_BUT_FIRST_ARG(__VA_ARGS__))
 
-#define TTYPE_INITIALIZATION(name, baseType, ptrType, _namespace, baseTypeArguments, ...) _TYPE_INITIALIZATION(name, baseType, MMAKE_PARAMETERS(ptrType, _namespace, (), baseTypeArguments, __VA_ARGS__))
-#define TYPE_INITIALIZATION(name, baseType, ptrType, _namespace, genericTypeArguments, baseTypeArguments, ...) _TYPE_INITIALIZATION(name, baseType, MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, baseTypeArguments, __VA_ARGS__))
+#define TTYPE_INITIALIZATION(name, baseType, ptrType, _namespace, genericTypeArguments, baseTypeArguments, ...) _TYPE_INITIALIZATION(name, baseType, MMAKE_PARAMETERS(ptrType, _namespace, genericTypeArguments, baseTypeArguments, __VA_ARGS__))
+#define TYPE_INITIALIZATION(name, baseType, ptrType, _namespace, baseTypeArguments, ...) _TYPE_INITIALIZATION(name, baseType, MMAKE_PARAMETERS(ptrType, _namespace, (), baseTypeArguments, __VA_ARGS__))
 
 #endif WINCOPIES_MACROS_TYPE_BASE_HXX
