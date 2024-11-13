00df8a12eed2948e48a62f9e72fcf30f323c51bf
Pierre Sprimont <49271108+pierresprim@users.noreply.github.com>
2024-11-13 19:34:07 +0100

    Misc updates

--- Source/Include/Util/Nullable.h
+++ Source/Include/Util/Nullable.h
@@ -16,14 +16,22 @@ namespace WinCopies
 		T _value;
 
 	public:
-		Nullable() = default;
-		INLINE_FIELD_SET(Nullable, T& const, value)
+		explicit Nullable()
+		{
+			_hasValue = false;
+			_value = NULL;
+		}
+		explicit Nullable(T const value)
+		{
+			_hasValue = true;
+			_value = value;
+		}
 
 		IINLINE_FIELD_RETURN(VIRTUALITY_NONE, bool, HasValue, hasValue)
 
-		GET_FIELD_IF_ELSE(ErrorCode, TryGetValue, T, value, _hasValue, ErrorCode::Success, default(T), ErrorCode::EmptyObject)
+		GET_FIELD_IF_ELSE(ErrorCode, TryGetValue, T, value, _hasValue, ErrorCode::Success, NULL, ErrorCode::EmptyObject)
 
-		INLINE_METHOD_RETURN(1, VIRTUALITY_NONE, T, GetValueOrDefault, _hasValue ? _value : default(T))
+		INLINE_METHOD_RETURN(1, VIRTUALITY_NONE, T, GetValueOrDefault, _hasValue ? _value : NULL)
 		INLINE_METHOD_RETURN(1, VIRTUALITY_NONE, T, GetValueOrCustom, _hasValue ? _value : custom, T custom)
 
 		STRUCT_DESTRUCTOR_DEFINITION(Nullable);
